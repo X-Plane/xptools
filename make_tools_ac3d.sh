c778baa968cafb2c2419612a0c5ec6cd0d47158f
bsupnik <bsupnik>
2006-03-08 14:29:09 +0000

    renamed scripts

---
+++ make_tools_ac3d.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+### ac3d
+rm ../XPWebSite/scenery/tools/ac3d_xplane_$1.zip
+cd build/Release/XPlaneSupportMac.p.bundle/Contents/MacOS/
+zip ../../../../../../XPWebSite/scenery/tools/ac3d_xplane_$1.zip XPlaneSupportMac.p
+cd ../../../../../
+cd AC3D\ Plugins/
+zip -r ../../XPWebSite/scenery/tools/ac3d_xplane_$1.zip XPlaneSupportWin.p
+cd ..
+cd src/AC3DPlugins
+zip ../../../XPWebSite/scenery/tools/ac3d_xplane_$1.zip README
+cd ../..
