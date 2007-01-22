bca7d26a946a03b214a420630455c1024d416d4b
bsupnik <bsupnik>
2007-01-22 18:54:21 +0000

    cfurl based code

--- src/Utils/PlatformUtils.mac.cpp
+++ src/Utils/PlatformUtils.mac.cpp
@@ -35,6 +35,7 @@
 #include <string.h>
 
 static	OSErr		FSSpecToPathName(const FSSpec * inFileSpec, char * outPathname);
+static	OSErr		FSRefToPathName(const FSRef * inFileSpec, char * outPathname);
 
 /* Endian routines for the Mac use Apple's Endian macros. */
 
@@ -82,7 +83,7 @@ int		GetFilePathFromUser(
 		OSErr				err;
 		NavReplyRecord		reply;
 		NavDialogOptions	options;
-		FSSpec				fileSpec;
+		FSRef				fileSpec;
 		
 	reply.version = kNavReplyRecordVersion;
 	err = NavGetDefaultDialogOptions(&options);
@@ -125,12 +126,12 @@ int		GetFilePathFromUser(
 		goto bail;
 
 	/* Convert the result from an AEDesc to a Mac file spec. */
-	err = AEGetNthPtr(&reply.selection, 1, typeFSS, NULL, NULL, &fileSpec, sizeof(fileSpec), NULL);
+	err = AEGetNthPtr(&reply.selection, 1, typeFSRef, NULL, NULL, &fileSpec, sizeof(fileSpec), NULL);
 	if (err != noErr)
 		goto bail;
 
 	/* Then convert the FSSpec to a full path. */
-	err = FSSpecToPathName(&fileSpec, outFileName);
+	err = FSRefToPathName(&fileSpec, outFileName);
 	if (err != noErr)
 		goto bail;
 
@@ -237,43 +238,35 @@ int		ConfirmMessage(const char * inMsg, const char * proceedBtn, const char * ca
 
 OSErr	FSSpecToPathName(const FSSpec * inFileSpec, char * outPathname)
 {
-	short			vRefNum = inFileSpec->vRefNum;
-	long			startDirID = inFileSpec->parID;
-
-	CInfoPBRec		paramRec;
-	Str255			dirName;	/* This will contain the name of the directory we get info about. */
-	OSErr 			err = noErr;
-	
-	paramRec.dirInfo.ioCompletion = nil;
-	paramRec.dirInfo.ioNamePtr = (StringPtr)(&dirName);
-	paramRec.dirInfo.ioDrParID = startDirID;
-
-	/* Start by putting a directory separator and the file name on the path. */
-	outPathname[0] = ':';
-	memcpy(outPathname+1, inFileSpec->name+1, inFileSpec->name[0]);
-	outPathname[inFileSpec->name[0]+1] = 0;
-
-	do {
-		paramRec.dirInfo.ioVRefNum = vRefNum;
-		paramRec.dirInfo.ioFDirIndex = -1;
-		paramRec.dirInfo.ioDrDirID = paramRec.dirInfo.ioDrParID;
-		
-		if (!(err = PBGetCatInfoSync(&paramRec))) 
-		{
-			/* For each directory we get info about, prepend a : and the directory name.
-			   But for the root directory, do NOT prepend the colon. */
-			short	newPart = dirName[0] + ((paramRec.dirInfo.ioDrDirID != fsRtDirID) ? 1 : 0);
-			memmove(outPathname+newPart, outPathname, strlen(outPathname)+1);
-			if (paramRec.dirInfo.ioDrDirID != fsRtDirID)
-			{
-				outPathname[0] = ':';
-				memcpy(outPathname+1, dirName+1, dirName[0]);
-			} else 
-				memcpy(outPathname, dirName+1, dirName[0]);
-		}
-	} while ((err == noErr) && (paramRec.dirInfo.ioDrDirID != fsRtDirID));
-	return err;
+	FSRef ref;
+	OSErr err = FSpMakeFSRef(inFileSpec, &ref);
+	if (err != noErr) return err;
+	return FSRefToPathName(&ref, outPathname);
 }
+
+OSErr	FSRefToPathName(const FSRef * inFileRef, char * outPathname)
+{
+	CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, inFileRef);
+	if (url == NULL)	return -1;
+	
+	CFURLPathStyle st = kCFURLPOSIXPathStyle;
+	#if defined(__MWERKS__)
+	CFURLPathStyle st = kCFURLHFSPathStyle;
+	#endif
+	CFStringRef	str = CFURLCopyFileSystemPath(url, st);
+	CFRelease(url);
+	if (str == NULL)	return -1;
+	
+	CFIndex		len = CFStringGetLength(str);
+	CFIndex		got = CFStringGetBytes(str, CFRangeMake(0, len), kCFStringEncodingMacRoman, 0, 0, (UInt8*)outPathname, len, NULL);
+	outPathname[len] = 0;
+	#if !DEV
+	scribble risk
+	#endif
+	CFRelease(str);
+	return noErr;
+}
+	
 
 int DoSaveDiscardDialog(const char * inMessage1, const char * inMessage2)
 {
