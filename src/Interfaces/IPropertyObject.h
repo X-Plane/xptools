bda81eab14900575e951763bc5e7a3cf76cb6e5b
bsupnik <bsupnik>
2007-02-01 06:41:40 +0000

    new prop obj interface

---
+++ src/Interfaces/IPropertyObject.h
@@ -0,0 +1,50 @@
+#ifndef IPROPERTYOBJECT_H
+#define IPROPERTYOBJECT_H
+
+/*
+	IPropertyObject - THEORY OF OPERATION
+	
+	This is a simple interface that lets client UI code "edit" an object by named properties, which are self-described in a format
+	adequate for UI presentation.  The idea here is to describe the datamodel - not to spec UI.  At least, that's my rationalization.
+
+*/
+
+
+enum {
+	prop_Int,
+	prop_Double,
+	prop_String,	
+	prop_Bool,			// Returns as int
+	prop_Enum			// Returns as int
+};
+
+typedef	map<int,string>	PropertyDict_t;
+
+struct PropertyInfo_t {
+	int				can_edit;
+	int				prop_kind;	
+	string			prop_name;
+};
+
+struct	PropertyVal_t {
+	int			prop_kind;
+	int			int_val;
+	string		string_val;
+	double		double_val;
+};
+
+class IPropertyObject {
+public:
+
+	virtual	int			FindProperty(const char * in_prop)=0;
+	virtual int			CountProperties(void)=0;
+	virtual void		GetNthPropertyInfo(int n, PropertyInfo_t& info)=0;
+	virtual	void		GetNthPropertyDict(int n, PropertyDict_t& dict)=0;			// Ben says: dictionary ops are broken out (and one vs all lookup are split too) for performance.
+	virtual	void		GetNthPropertyDictItem(int n, int e, string& item)=0;		// It may be slow to get all enums, so give the UI code a way to say if it needs this info.
+	
+	virtual void		GetNthProperty(int n, PropertyVal_t& val)=0;
+	virtual void		SetNthProperty(int n, const PropertyVal_t& val)=0;
+	
+};
+
+#endif /* IPROPERTYOBJECT_H */
