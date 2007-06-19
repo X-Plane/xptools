318d266c0be173e0d24f30eba6a51603754cafd1
bsupnik <bsupnik>
2007-06-19 04:55:13 +0000

    version in about box

--- src/WEDWindows/WED_AboutBox.cpp
+++ src/WEDWindows/WED_AboutBox.cpp
@@ -23,8 +23,10 @@
 
 #include "WED_AboutBox.h"
 #include "GUI_DrawUtils.h"
+#include "GUI_Fonts.h"
 #include "GUI_Resources.h"
 #include "BitmapUtils.h"
+#include "WED_Version.h"
 
 static int *	SizeOfPng(const char * png)
 {
@@ -55,6 +57,21 @@ void		WED_AboutBox::Draw(GUI_GraphState * state)
 	int tile_sel[4] = { 0, 0, 1, 1 };
 	GUI_Pane::GetBounds(bounds);
 	GUI_DrawCentered(state, "about.png", bounds, 0, 0, tile_sel, NULL, NULL);
+	
+	char buf[1024];
+	
+	sprintf(buf,"WED " WED_VERSION_STRING ", compiled on "__DATE__" "__TIME__);
+	float color[4] = { 1.0, 1.0, 1.0, 1.0 };
+	
+	float f = GUI_GetLineHeight(font_UI_Basic);
+	
+	GUI_FontDrawScaled(state, font_UI_Basic, color, 
+		bounds[0],
+		bounds[0] * 0.75 + bounds[2] * 0.25 - f * 0.5,
+		bounds[2],
+		bounds[0] * 0.75 + bounds[2] * 0.25 + f * 0.5,
+		buf,buf+strlen(buf), align_Center);
+	
 }
 
 int			WED_AboutBox::MouseDown(int x, int y, int button)
