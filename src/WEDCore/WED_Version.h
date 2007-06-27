debba764ca345c0eb4df250b33e4666cd01450ef
bsupnik <bsupnik>
2007-06-27 04:55:07 +0000

    WED a3

--- src/WEDCore/WED_Version.h
+++ src/WEDCore/WED_Version.h
@@ -24,9 +24,9 @@
 #ifndef WED_Version_H
 #define WED_Version_H
 
-#define	WED_VERSION			 1.0a2
-#define	WED_VERSION_STRING	"1.0a2"
-#define	WED_VERSION_RES		"1.0a2"
-#define	WED_VERSION_BIN		1,0,0,2
+#define	WED_VERSION			 1.0a3
+#define	WED_VERSION_STRING	"1.0a3"
+#define	WED_VERSION_RES		"1.0a3"
+#define	WED_VERSION_BIN		1,0,0,3
 
 #endif /* WED_Version_H */
