2a7aa8f37f379dcb4e5964955c9dd5970c41b0b8
bsupnik <bsupnik>
2007-07-03 14:22:19 +0000

    doc opening moved to start window

--- src/WEDCore/WED_Application.cpp
+++ src/WEDCore/WED_Application.cpp
@@ -5,19 +5,7 @@
 #include "GUI_Help.h"
 #include "WED_UIDefs.h"
 #include "WED_Menus.h"
-
-const char * LastPart(const char * s)
-{
-	const char * ret = s;
-	while (*s)
-	{
-		if (*s==':'||*s=='/'||*s=='\\') ret=s;
-		++s;		
-	}
-	return ret;
-}
-
-
+#include "WED_PackageMgr.h"
 
 WED_Application::WED_Application() : mAboutBox(NULL)
 {
@@ -46,38 +34,6 @@ int		WED_Application::HandleCommand(int command)
 	case wed_HelpScenery:
 		GUI_LaunchURL("http://scenery.x-plane.com/");
 		return 1;
-	case wed_NewPackage:
-		if (GetFilePathFromUser(getFile_Save, "Please name your new scenery package", "Create", FILE_DIALOG_NEW_PROJECT, buf, sizeof(buf)))
-		{
-			try {
-			
-				double b[4] = { -180, -90, 180, 90 };
-				WED_Document * doc = new WED_Document(buf, b);				
-				WED_DocumentWindow * wind = new WED_DocumentWindow(LastPart(buf), this, doc);
-			} catch (...) {
-				DoUserAlert("An error occurred");
-				#if ERROR_HANDLING
-				fix this					
-				#endif
-			}
-		}
-		return 1;
-	case wed_OpenPackage:
-		if (GetFilePathFromUser(getFile_PickFolder, "Please pick your scenery package", "Open", FILE_DIALOG_OPEN_PROJECT, buf, sizeof(buf) ))
-		{
-			try {
-				double b[4] = { -180, -90, 180, 90 };
-				WED_Document * doc = new WED_Document(buf, b);				
-				WED_DocumentWindow * wind = new WED_DocumentWindow(LastPart(buf), this, doc);
-
-			} catch (...) {
-				DoUserAlert("An error occurred");
-				#if ERROR_HANDLING
-				fix this					
-				#endif
-			}
-		}
-		return 1;
 	default:
 		return GUI_Application::HandleCommand(command);
 	}
@@ -89,8 +45,6 @@ int		WED_Application::CanHandleCommand(int command, string& ioName, int& ioCheck
 	case gui_Undo:		ioName = "&Undo"; return 0;
 	case gui_Redo:		ioName = "&Redo"; return 0;
 	case wed_HelpScenery:
-	case wed_NewPackage:
-	case wed_OpenPackage:	return 1;
 	default:				return GUI_Application::CanHandleCommand(command, ioName, ioCheck);
 	}
 }
