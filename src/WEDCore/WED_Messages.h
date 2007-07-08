fc7f69d987df3ff0f5893354c3df579320598dad
bsupnik <bsupnik>
2007-07-08 03:04:01 +0000

    new msgs

--- src/WEDCore/WED_Messages.h
+++ src/WEDCore/WED_Messages.h
@@ -9,12 +9,15 @@ enum {
 	msg_PackageDestroyed = GUI_APP_MESSAGES,
 	msg_DocumentDestroyed,
 	
-	msg_LayerStatusChanged,					// Sent when layer flags are toggled, renamed, whatever
-	msg_LayerCountChanged,					// Sent when number of layers changes
+//	msg_LayerStatusChanged,					// Sent when layer flags are toggled, renamed, whatever
+//	msg_LayerCountChanged,					// Sent when number of layers changes
 
-	msg_SelectionChanged,
+//	msg_SelectionChanged,
 	
 	msg_ArchiveChanged,
+	
+	msg_DocWillSave,
+	msg_DocLoaded,
 	
 	msg_SystemFolderChanged,
 	msg_SystemFolderUpdated	
