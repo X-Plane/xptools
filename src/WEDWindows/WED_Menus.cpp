5280f301e713f3402390d780702674d0cce64e99
bsupnik <bsupnik>
2007-07-24 20:09:04 +0000

    turned off DSF export for now

--- src/WEDWindows/WED_Menus.cpp
+++ src/WEDWindows/WED_Menus.cpp
@@ -23,7 +23,7 @@ static const GUI_MenuItem_t	kFileMenu[] = {
 {	"&Validate",			'V',	gui_ControlFlag+gui_ShiftFlag,	0,	wed_Validate		},
 {	"&Import apt.dat...",	'I',	gui_ControlFlag+gui_ShiftFlag,	0,	wed_ImportApt		},
 {	"&Export apt.dat...",	'S',	gui_ControlFlag+gui_ShiftFlag,	0,	wed_ExportApt		},
-#if DEV
+#if 0
 {	"Export &DSFs",			0,		0,								0,	wed_ExportDSF		},
 #endif
 #if IBM
