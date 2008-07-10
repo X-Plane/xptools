ff581c2e48c66ca47ac7b44c3eaa2d02196e9057
bsupnik <bsupnik>
2008-07-10 11:43:25 +0000

    r1 version, etc.

--- src/WEDCore/WED_Version.h
+++ src/WEDCore/WED_Version.h
@@ -24,9 +24,9 @@
 #ifndef WED_Version_H
 #define WED_Version_H
 
-#define	WED_VERSION			 1.0b5
-#define	WED_VERSION_STRING	"1.0b5"
-#define	WED_VERSION_RES		"1.0b5"
-#define	WED_VERSION_BIN		1,0,0,12
+#define	WED_VERSION			 1.0r1
+#define	WED_VERSION_STRING	"1.0r1"
+#define	WED_VERSION_RES		"1.0r1"
+#define	WED_VERSION_BIN		1,0,0,13
 
 #endif /* WED_Version_H */
