4eecd74264d9784e907672e1c7d8164c6d935f66
sothis <sothis>
2009-04-01 17:44:31 +0000

    clean up Makefiles
    - initial changes, need to change other makerules accordingly yet
    - add -Wno-multichar to CFLAGS to shut up gcc, we know what we're doing,
      do we? :-)
    - simplifying platform if's. instead of using "ifeq ($(PLATFORM), Linux)" we
      use "ifdef PLAT_LINUX" now. the same applies for architecture dependent
      parts,for example "ifdef ARCH_I368".
    - cleaning echo strings to improve display on 80-char terminals
    - using "+=" to append sources, include paths, etc to get rid of ugly
      backspaces and to make copy&paste easier. also allows easier exlusion of
      specific lines by commenting them out (see makerules/WED for an example)
    - prepend "./" to relative file paths to emphasize their relativeness
    - add callgrind.out.* pattern to gitignore, remove deprecated patterns
    
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BUILDDIR	:= build
+BUILDDIR	:= ./build
 LIBRARIES	:= ./libs/local/.xpt_libs
 # default to public debug_opt configuration
 ifndef conf
@@ -10,60 +10,48 @@ ifneq (, $(findstring MINGW, $(PLATFORM)))
 endif
 TARGETDIR	:= $(BUILDDIR)/$(PLATFORM)/$(conf)
 
-.SILENT:
-.PHONY: all
-.PHONY: clean
-.PHONY: distclean
-
-.PHONY: libs
-.PHONY: ObjView
-.PHONY: WED
-.PHONY: DSFTool
-.PHONY: DDSTool
-.PHONY: ObjConverter
-.PHONY: MeshTool
-.PHONY: RenderFarm
-.PHONY: fonttool
+.PHONY: all clean distclean libs ObjView WED DSFTool DDSTool ObjConverter \
+MeshTool RenderFarm fonttool
 
 all: WED MeshTool ObjView DSFTool DDSTool ObjConverter RenderFarm
 
 libs: $(LIBRARIES)
 
 $(LIBRARIES):
-	$(MAKE) -C "./libs" all
+	@$(MAKE) -C "./libs" all
 
 ObjView: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 WED: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 DSFTool: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 DDSTool: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 ObjConverter: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 MeshTool: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 XGrinder: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 RenderFarm: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 fonttool: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -f makerules/rules.mk
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/$@ -s -f makerules/rules.mk
 
 ac3d: libs
-	$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/XPlaneSupportLin.p -f makerules/rules.mk
-	
+	@$(MAKE) conf=$(conf) cross=$(cross) BUILDDIR=$(TARGETDIR) TARGET=$(TARGETDIR)/XPlaneSupportLin.p -s -f makerules/rules.mk
+
 clean:
-	-rm -rf $(BUILDDIR)
+	@-rm -rf $(BUILDDIR)
 
 distclean:	clean
-	$(MAKE) -C "./libs" clean
+	@$(MAKE) -C "./libs" clean
--- makerules/rules.mk
+++ makerules/rules.mk
@@ -4,73 +4,74 @@
 
 .PHONY: all
 
-PLATFORM		:= $(shell uname)
+PLATFORM	:= $(shell uname)
 ARCHITECTURE	:= $(shell uname -m)
-WD				:= $(PWD)
+WD		:= $(PWD)
 ifneq (, $(findstring MINGW, $(PLATFORM)))
 	PLATFORM	:= Mingw
+	PLAT_MINGW	:= Yes
 endif
+ifeq ($(PLATFORM), Linux)
+	PLAT_LINUX	:= Yes
+endif
+ifeq ($(PLATFORM), Darwin)
+	PLAT_DARWIN	:= Yes
+endif
+
+ifeq ($(ARCHITECTURE), i386)
+	ARCH_I386	:= Yes
+endif
+ifeq ($(ARCHITECTURE), i686)
+	ARCH_I386	:= Yes
+endif
+ifeq ($(ARCHITECTURE), x86_64)
+	ARCH_X86_64	:= Yes
+endif
+
 ifeq ($(cross), mingw64)
-	MULTI_PREFIX	:= 64_
 	MULTI_SUFFIX	:= 64
-	CROSSPREFIX		:= x86_64-pc-mingw32-
+	CROSSPREFIX	:= x86_64-pc-mingw32-
 	ARCHITECTURE	:= x86_64
 endif
 ifeq ($(cross), m32)
-	MULTI_PREFIX	:= 32_
 	MULTI_SUFFIX	:= 32
-	M32_SWITCH		:= -m32
+	M32_SWITCH	:= -m32
 endif
 
-ifeq ($(PLATFORM), Darwin)
-	ECHOFLAGS	:=
-else
+ifndef PLAT_DARWIN
 	ECHOFLAGS	:= -e
 endif
 
-ifdef NOTCOLORED
-print_clean		:= echo "[ deleting all generated files  ]"
-print_link		:= echo "[ linking executable      ]: "
-print_comp_cc	:= echo "[ compiling c object      ]: "
-print_comp_cxx	:= echo "[ compiling cpp object    ]: "
-print_comp_res	:= echo "[ generating resource     ]: "
-print_arch		:= echo "[ creating static library ]: "
-print_so		:= echo "[ creating shared library ]: "
-print_dep		:= echo "[ calculating dependency  ]: "
-print_finished	:= echo " finished."
-print_error		:= (echo "[ --FAILED-- ]" && false)
-else
-clearscreen		:= echo $(ECHOFLAGS) "\033[2J\033[H"
-print_clean		:= echo $(ECHOFLAGS) "\033[0;31m[ deleting all generated files  ]\033[0m"
-print_link		:= echo $(ECHOFLAGS) "\033[0;34m[ linking executable      ]\033[0m: "
-print_comp_cc	:= echo $(ECHOFLAGS) "\033[0;32m[ compiling c object      ]\033[0m: "
-print_comp_cxx	:= echo $(ECHOFLAGS) "\033[0;32m[ compiling cpp object    ]\033[0m: "
-print_comp_res	:= echo $(ECHOFLAGS) "\033[0;32m[ generating resource     ]\033[0m: "
-print_arch		:= echo $(ECHOFLAGS) "\033[0;34m[ creating static library ]\033[0m: "
-print_so		:= echo $(ECHOFLAGS) "\033[0;34m[ creating shared library ]\033[0m: "
-print_dep		:= echo $(ECHOFLAGS) "\033[0;34m[ calculating dependency  ]\033[0m: "
+clearscreen	:= echo $(ECHOFLAGS) "\033[2J\033[H"
+print_clean	:= echo $(ECHOFLAGS) "\033[0;31m[ RM  ]\033[0m"
+print_link	:= echo $(ECHOFLAGS) "\033[0;34m[ LD  ]\033[0m:"
+print_comp_cc	:= echo $(ECHOFLAGS) "\033[0;32m[ CC  ]\033[0m:"
+print_comp_cxx	:= echo $(ECHOFLAGS) "\033[0;32m[ CXX ]\033[0m:"
+print_comp_res	:= echo $(ECHOFLAGS) "\033[0;32m[ RES ]\033[0m:"
+print_arch	:= echo $(ECHOFLAGS) "\033[0;34m[ AR  ]\033[0m:"
+print_so	:= echo $(ECHOFLAGS) "\033[0;34m[ SO  ]\033[0m:"
+print_dep	:= echo $(ECHOFLAGS) "\033[0;34m[ DEP ]\033[0m:"
 print_finished	:= echo $(ECHOFLAGS) "\033[0;32m finished.\033[0m"
-print_error		:= (echo $(ECHOFLAGS) "\033[0;31m[ --FAILED-- ]\033[0m" && false)
-endif
+print_error	:= (echo $(ECHOFLAGS) "\033[0;31m[ --FAILED-- ]\033[0m" && false)
 
 
 ##
 # tools
 #############################
 
-ifeq ($(PLATFORM), Darwin)
-CC			:= gcc-4.2
-CXX			:= g++-4.2
-LD			:= g++-4.2
-AR			:= libtool
+ifdef PLAT_DARWIN
+CC	:= gcc-4.2
+CXX	:= g++-4.2
+LD	:= g++-4.2
+AR	:= libtool
 else
-CC			:= $(CROSSPREFIX)gcc
-CXX			:= $(CROSSPREFIX)g++
-LD			:= $(CROSSPREFIX)g++
-AR			:= $(CROSSPREFIX)ar
-OBJCOPY		:= $(CROSSPREFIX)objcopy
-STRIP		:= $(CROSSPREFIX)strip
-WINDRES		:= $(CROSSPREFIX)windres
+CC	:= $(CROSSPREFIX)gcc
+CXX	:= $(CROSSPREFIX)g++
+LD	:= $(CROSSPREFIX)g++
+AR	:= $(CROSSPREFIX)ar
+OBJCOPY	:= $(CROSSPREFIX)objcopy
+STRIP	:= $(CROSSPREFIX)strip
+WINDRES	:= $(CROSSPREFIX)windres
 endif
 
 
@@ -78,7 +79,7 @@ endif
 # target specific environment
 #############################
 
-include makerules/$(shell basename $(TARGET))
+include ./makerules/$(shell basename $(TARGET))
 REAL_TARGET := $(TARGET)$(MULTI_SUFFIX)
 
 
@@ -86,25 +87,23 @@ REAL_TARGET := $(TARGET)$(MULTI_SUFFIX)
 # architecture specific environment
 ###################################
 
-ifeq ($(ARCHITECTURE), i386)
+
+ifdef ARCH_I386
 	OBJFORMAT = elf32-i386
 	BINFORMAT = i386
 endif
-ifeq ($(ARCHITECTURE), i686)
-	OBJFORMAT = elf32-i386
-	BINFORMAT = i386
-endif
-ifeq ($(ARCHITECTURE), x86_64)
+ifdef ARCH_X86_64
 	OBJFORMAT = elf64-x86-64
 	BINFORMAT = i386:x86-64
 endif
-ifeq ($(PLATFORM), Mingw)
+
+ifdef PLAT_MINGW
 # uname -m gives i686, regardless if on win32 or win64
-ifeq ($(ARCHITECTURE), i686)
+ifdef ARCH_I386
 	OBJFORMAT = pei-i386
 	BINFORMAT = i386
 endif
-ifeq ($(ARCHITECTURE), x86_64)
+ifdef ARCH_X86_64
 	OBJFORMAT = pei-x86-64
 	BINFORMAT = i386
 endif
@@ -115,23 +114,26 @@ endif
 # platform specific environment
 ###############################
 
-ifeq ($(PLATFORM), Linux)
+ifdef PLAT_LINUX
+# if someone has a ppc linux machine, please define -DLIL/-DBIG in the code,
+# remove them here and use the __ppc__ macro to resolve endianess issues
 	DEFINES		:= $(DEFINES) -DLIN=1 -DIBM=0 -DAPL=0 -DLIL=1 -DBIG=0
-	CFLAGS		:= $(CFLAGS) $(M32_SWITCH) -fvisibility=hidden
-	CXXFLAGS	:= $(CXXFLAGS) $(M32_SWITCH) -fvisibility=hidden -Wno-deprecated
+	CFLAGS		:= $(CFLAGS) $(M32_SWITCH) -fvisibility=hidden -Wno-multichar
+	CXXFLAGS	:= $(CXXFLAGS) $(M32_SWITCH) -fvisibility=hidden -Wno-deprecated -Wno-multichar
 	LDFLAGS		:= $(LDFLAGS) $(M32_SWITCH) -rdynamic
 endif
-ifeq ($(PLATFORM), Darwin)
+ifdef PLAT_DARWIN
+# -DLIL/-DBIG have to be defined in the code itself to support universal builds
 	DEFINES		:= $(DEFINES) -DLIN=0 -DIBM=0 -DAPL=1
-	CXXFLAGS	:= $(CXXFLAGS) -fvisibility=hidden -mmacosx-version-min=10.4
-	CFLAGS		:= $(CFLAGS) -fvisibility=hidden -mmacosx-version-min=10.4
+	CXXFLAGS	:= $(CXXFLAGS) -fvisibility=hidden -mmacosx-version-min=10.4 -Wno-multichar
+	CFLAGS		:= $(CFLAGS) -fvisibility=hidden -mmacosx-version-min=10.4 -Wno-multichar
 	LDFLAGS		:= $(LDFLAGS) -mmacosx-version-min=10.4 -Z
 	MACARCHS	:= -arch i386 -arch ppc
 endif
-ifeq ($(PLATFORM), Mingw)
+ifdef PLAT_MINGW
 	DEFINES		:= $(DEFINES) -DLIN=0 -DIBM=1 -DAPL=0 -DLIL=1 -DBIG=0
-	CFLAGS		:= $(CFLAGS)
-	CXXFLAGS	:= $(CXXFLAGS) -Wno-deprecated
+	CFLAGS		:= $(CFLAGS) -Wno-multichar
+	CXXFLAGS	:= $(CXXFLAGS) -Wno-deprecated -Wno-multichar
 	LDFLAGS		:= $(LDFLAGS)
 endif
 
@@ -183,13 +185,12 @@ endif
 
 CXXSOURCES	:= $(filter %.cpp, $(SOURCES))
 CCSOURCES	:= $(filter %.c, $(SOURCES))
-CCDEPS		:= $(patsubst %.c, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep, $(CCSOURCES))
-CCOBJECTS	:= $(patsubst %.c, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).o, $(CCSOURCES))
-CXXDEPS		:= $(patsubst %.cpp, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep, $(CXXSOURCES))
-CXXOBJECTS	:= $(patsubst %.cpp, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).o, $(CXXSOURCES))
-RESOURCEOBJ	:= $(patsubst %, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).ro, $(RESOURCES))
-WIN_RESOURCEOBJ	:= $(patsubst %.rc, $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).res, $(WIN_RESOURCES))
-
+CCDEPS		:= $(patsubst %.c, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep, $(CCSOURCES))
+CCOBJECTS	:= $(patsubst %.c, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).o, $(CCSOURCES))
+CXXDEPS		:= $(patsubst %.cpp, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep, $(CXXSOURCES))
+CXXOBJECTS	:= $(patsubst %.cpp, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).o, $(CXXSOURCES))
+RESOURCEOBJ	:= $(patsubst %, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).ro, $(RESOURCES))
+WIN_RESOURCEOBJ	:= $(patsubst %.rc, $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).res, $(WIN_RESOURCES))
 
 
 ##
@@ -200,40 +201,40 @@ all: $(REAL_TARGET)
 
 -include $(CCDEPS) $(CXXDEPS)
 
-$(REAL_TARGET):  $(CCOBJECTS) $(CXXOBJECTS) $(CCDEPS) $(CXXDEPS) $(RESOURCEOBJ) $(WIN_RESOURCEOBJ)
-	-mkdir -p $(dir $(@))
+$(REAL_TARGET): $(CCOBJECTS) $(CXXOBJECTS) $(CCDEPS) $(CXXDEPS) $(RESOURCEOBJ) $(WIN_RESOURCEOBJ)
+	@-mkdir -p $(dir $(@))
 
 # static library
 
 ifeq ($(TYPE), LIBSTATIC)
-	$(print_arch) $@
+	@$(print_arch) $(subst $(PWD)/, ./, $(abspath $(@)))
 ifeq ($(PLATFORM), Darwin)
-	$(AR) -static -o $@ $(ARFLAGS) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) || $(print_error)
+	@$(AR) -static -o $(@) $(ARFLAGS) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) || $(print_error)
 else
-	$(AR) $(ARFLAGS) $@ $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) || $(print_error)
+	@$(AR) $(ARFLAGS) $(@) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) || $(print_error)
 endif
 endif
 
 # shared library
 
 ifeq ($(TYPE), LIBDYNAMIC)
-	$(print_so) $@
+	@$(print_so) $(subst $(PWD)/, ./, $(abspath $(@)))
 ifeq ($(PLATFORM), Linux)
-	$(LD) $(LDFLAGS) $(LIBPATHS) -rdynamic -shared -Wl,-export-dynamic,-soname,$(notdir $(@)) -o $@ $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
+	@$(LD) $(LDFLAGS) $(LIBPATHS) -rdynamic -shared -Wl,-export-dynamic,-soname,$(notdir $(@)) -o $(@) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
 endif
 ifeq ($(PLATFORM), Darwin)
-	$(LD) $(MACARCHS) $(LDFLAGS) $(LIBPATHS) -rdynamic -shared -o $@ $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
+	@$(LD) $(MACARCHS) $(LDFLAGS) $(LIBPATHS) -rdynamic -shared -o $(@) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
 endif
 ifeq ($(PLATFORM), Mingw)
-	$(LD) $(LDFLAGS) $(LIBPATHS) -shared -Wl,-export-dynamic,-soname,$(notdir $(@)) -o $@ $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
+	@$(LD) $(LDFLAGS) $(LIBPATHS) -shared -Wl,-export-dynamic,-soname,$(notdir $(@)) -o $(@) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
 endif
 endif
 
 # executable
 
 ifeq ($(TYPE), EXECUTABLE)
-	$(print_link) $@
-	$(LD) $(MACARCHS) $(LDFLAGS) $(LIBPATHS) -o $@ $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
+	@$(print_link) $(subst $(PWD)/, ./, $(abspath $(@)))
+	@$(LD) $(MACARCHS) $(LDFLAGS) $(LIBPATHS) -o $(@) $(CCOBJECTS) $(CXXOBJECTS) $(WIN_RESOURCEOBJ) $(RESOURCEOBJ) $(LIBS) $(STDLIBS) || $(print_error)
 endif
 
 # debug information
@@ -241,50 +242,50 @@ endif
 ifneq ($(TYPE), LIBSTATIC)
 ifeq ($(conf), release_opt)
 ifeq ($(PLATFORM), Linux)
-	$(OBJCOPY) --only-keep-debug $@ $(@).debug
-	$(STRIP) -s -x $@
-	cd  $(dir $(@)) && $(OBJCOPY) --add-gnu-debuglink=$(notdir $(@)).debug $(notdir $(@)) && cd $(WD)
-	chmod 0644 $(@).debug
+	@$(OBJCOPY) --only-keep-debug $(@) $(@).debug
+	@$(STRIP) -s -x $(@)
+	@cd  $(dir $(@)) && $(OBJCOPY) --add-gnu-debuglink=$(notdir $(@)).debug $(notdir $(@)) && cd $(WD)
+	@chmod 0644 $(@).debug
 endif
 ifeq ($(PLATFORM), Mingw)
-	$(OBJCOPY) --only-keep-debug $@.exe $(@).debug
-	$(STRIP) -s -x $@.exe
-	cd  $(dir $(@)) && $(OBJCOPY) --add-gnu-debuglink=$(notdir $(@)).debug $(notdir $(@)).exe && cd $(WD)
-	chmod 0644 $(@).debug
+	@$(OBJCOPY) --only-keep-debug $(@).exe $(@).debug
+	@$(STRIP) -s -x $(@).exe
+	@cd  $(dir $(@)) && $(OBJCOPY) --add-gnu-debuglink=$(notdir $(@)).debug $(notdir $(@)).exe && cd $(WD)
+	@chmod 0644 $(@).debug
 endif
 #TODO: add Darwin, at least strip binaries
 endif
 endif
-	$(print_finished)
+	@$(print_finished)
 
 # basic rules
 
-$(RESOURCEOBJ): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).ro : %
-	$(print_comp_res) $<
-	-mkdir -p $(dir $(@))
-	cd  $(dir $(<)) && $(OBJCOPY) -I binary -O $(OBJFORMAT) -B $(BINFORMAT) $(notdir $(<)) $(WD)/$(@) && cd $(WD)
+$(RESOURCEOBJ): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).ro : %
+	@$(print_comp_res) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@cd $(dir $(<)) && $(OBJCOPY) -I binary -O $(OBJFORMAT) -B $(BINFORMAT) $(notdir $(<)) $(WD)/$(@) && cd $(WD)
 
-$(WIN_RESOURCEOBJ): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).res : %.rc
-	$(print_comp_res) $<
-	-mkdir -p $(dir $(@))
-	$(WINDRES) $< -O coff -o $@
+$(WIN_RESOURCEOBJ): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).res : %.rc
+	@$(print_comp_res) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@$(WINDRES) $< -O coff -o $(@)
 
-$(CCOBJECTS): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).o: %.c $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep
-	$(print_comp_cc) $<
-	-mkdir -p $(dir $(@))
-	$(CC) $(MACARCHS) $(CFLAGS) $(DEFINES) $(INCLUDEPATHS) -c $< -o $@ || $(print_error)
+$(CCOBJECTS): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).o: %.c $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep
+	@$(print_comp_cc) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@$(CC) $(MACARCHS) $(CFLAGS) $(DEFINES) $(INCLUDEPATHS) -c $(<) -o $(@) || $(print_error)
 
-$(CXXOBJECTS): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).o: %.cpp $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep
-	$(print_comp_cxx) $<
-	-mkdir -p $(dir $(@))
-	$(CXX) $(MACARCHS) $(CXXFLAGS) $(DEFINES) $(INCLUDEPATHS) -c $< -o $@ || $(print_error)
+$(CXXOBJECTS): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).o: %.cpp $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep
+	@$(print_comp_cxx) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@$(CXX) $(MACARCHS) $(CXXFLAGS) $(DEFINES) $(INCLUDEPATHS) -c $(<) -o $(@) || $(print_error)
 
-$(CCDEPS): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep: %.c
-	$(print_dep) $<
-	-mkdir -p $(dir $(@))
-	$(CC) $(CFLAGS) $(DEFINES) $(INCLUDEPATHS) -MM $< -MT $@ -MT $(<:.c=.o) -o $@ || $(print_error)
+$(CCDEPS): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep: %.c
+	@$(print_dep) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@$(CC) $(CFLAGS) $(DEFINES) $(INCLUDEPATHS) -MM $< -MT $(@) -MT $(<:.c=.o) -o $(@) || $(print_error)
 
-$(CXXDEPS): $(BUILDDIR)/$(MULTI_PREFIX)%$(FORCEREBUILD_SUFFIX).dep: %.cpp
-	$(print_dep) $<
-	-mkdir -p $(dir $(@))
-	$(CXX) $(CXXFLAGS) $(DEFINES) $(INCLUDEPATHS) -MM $< -MT $@ -MT $(<:.cpp=.o) -o $@ || $(print_error)
+$(CXXDEPS): $(BUILDDIR)/%$(FORCEREBUILD_SUFFIX)$(MULTI_SUFFIX).dep: %.cpp
+	@$(print_dep) $(subst $(PWD)/, ./, $(abspath $(<)))
+	@-mkdir -p $(dir $(@))
+	@$(CXX) $(CXXFLAGS) $(DEFINES) $(INCLUDEPATHS) -MM $< -MT $(@) -MT $(<:.cpp=.o) -o $(@) || $(print_error)
