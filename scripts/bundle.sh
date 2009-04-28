7385cdc66deb2f1cdd8818be2103e8ba2da45111
bsupnik <bsupnik>
2009-04-28 19:55:16 +0000

    Fixing windows build

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -10,14 +10,17 @@ case "$2" in
 mac)
 	build_dir=../build/Release
 	suffix=Mac
+	tsuffix=""
 	;;
 win)
 	build_dir=../build/Mingw/release_opt
 	suffix=Win
+	tsuffix=".exe"
 	;;
 lin)
 	build_dir=../build/Linux/release_opt
 	suffix=Lin
+	tsuffix=""
 	;;
 esac
 
@@ -29,14 +32,14 @@ case "$1" in
 ac3d)
 	out_file="ac3d_plugin_"$2"_"$3".zip"
 	src_files="$build_dir/XPlaneSupport$suffix.p ../src/AC3DPlugins/README ../src/AC3DPlugins/XPlaneSupport.tcl"
-	if [ "$2" != "mac" ]; then
+	if [ "$2" == "lin" ]; then
 		src_files="$src_files $build_dir/XPlaneSupport$suffix.p.debug"
 	fi
 	;;
 meshtool)
 	out_file="meshtool_"$2"_"$3".zip"
-	src_files="$build_dir/MeshTool ../src/MeshTool/README.meshtool"
-	if [ "$2" != "mac" ]; then
+	src_files="$build_dir/MeshTool$tsuffix ../src/MeshTool/README.meshtool"
+	if [ "$2" == "lin" ]; then
 		src_files="$src_files $build_dir/MeshTool.debug"
 	fi
 	;;
