8fab69f826fdf8ef484de7e16b00fd881ccd2adf
bsupnik <bsupnik>
2009-05-08 16:00:17 +0000

    checking meshtool config files in.

---
+++ src/MeshTool/config/dsf2text.cmd
@@ -0,0 +1,3 @@
+CMD .env .dsf "../build/Debug/DSFTool" -env2overlay "INFILE" "OUTFILE"
+CMD .txt .dsf "../build/Debug/DSFTool" -text2dsf "INFILE" "OUTFILE"
+CMD .dsf .txt "../build/Debug/DSFTool" -dsf2text "INFILE" "OUTFILE"
