f2eb11b38c21614bfc05bdfb70ef7576a081e47c
bsupnik <bsupnik>
2009-10-05 18:42:18 +0000

    packaging fix for mac

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,25 +2,37 @@
 
 # usage: bundle.sh product platform version
 
+cd ..
+home_dir=`pwd`
+
+# home is root of XPTools
+# build dir is rel path to final binary objs from home
+# src_files are all bin objects relative to build dir
+# doc_files are all others relative to home
+# out_file is name of final, ends up in scripts
+# WHY is it like this?   Mac apps are freaking folder trees - so we need to be IN the build dir 
+# to recursively zip the app.  The "doc files" is everything else, assumed single file, so thus we
+# can just zip -j to discard the paths.
+
 case "$2" in
 "")
 	echo "Second parameter must be mac|win|lin"
 	exit 1
 	;;
 mac)
-	build_dir=../build/Release
+	build_dir=build/Release
 	suffix=Mac
 	tsuffix=""
 	asuffix=".app"
 	;;
 win)
-	build_dir=../build/Mingw/release_opt
+	build_dir=build/Mingw/release_opt
 	suffix=Win
 	tsuffix=".exe"
 	asuffix=".exe"
 	;;
 lin)
-	build_dir=../build/Linux/release_opt
+	build_dir=build/Linux/release_opt
 	suffix=Lin
 	tsuffix=""
 	asuffix=""
@@ -34,24 +46,32 @@ case "$1" in
 	;;
 ac3d)
 	out_file="ac3d_plugin_"$2"_"$3".zip"
-	src_files="$build_dir/XPlaneSupport$suffix.p ../src/AC3DPlugins/README ../src/AC3DPlugins/XPlaneSupport.tcl"
+	src_files="XPlaneSupport$suffix.p"
+	doc_files="$build_dir/XPlaneSupport$suffix.p src/AC3DPlugins/README src/AC3DPlugins/XPlaneSupport.tcl"
 	if [ "$2" == "lin" ]; then
-		src_files="$src_files $build_dir/XPlaneSupport$suffix.p.debug"
+		doc_files="doc_files $build_dir/XPlaneSupport$suffix.p.debug"
 	fi
 	;;
 meshtool)
 	out_file="meshtool_"$2"_"$3".zip"
-	src_files="$build_dir/MeshTool$tsuffix ../src/MeshTool/README.meshtool"
+	src_files="MeshTool$tsuffix"
+	doc_files="src/MeshTool/README.meshtool"
 	if [ "$2" == "lin" ]; then
-		src_files="$src_files $build_dir/MeshTool.debug"
+		doc_files="$doc_files $build_dir/MeshTool.debug"
 	fi
 	;;
 wed)
 	out_file="wed_"$2"_"$3".zip"
-	src_files="$build_dir/WED$asuffix ../src/WEDResources/WEDManual.pdf"
+	src_files="WED$asuffix"
+	doc_files="src/WEDResources/WEDManual.pdf"
 	;;
 esac
 
+cd "$home_dir/scripts"
 rm $out_file
-zip -j $out_file $src_files
+cd ../$build_dir
+zip -r "$home_dir/scripts/$out_file" "$src_files"
+cd "$home_dir"
+zip -j "$home_dir/scripts/$out_file" $doc_files
+cd "$home_dir/scripts"
 scp $out_file bsupnik@dev.x-plane.com:/shared/download/tools/
