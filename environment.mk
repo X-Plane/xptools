c20a4547fe7bdeb7824b9a10f36da3d1a18e5213
sothis <sothis>
2011-10-29 10:50:43 +0000

    make multilib check reliable
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- environment.mk
+++ environment.mk
@@ -1,18 +1,6 @@
 ARCHITECTURE	:= $(shell uname -m)
 PLATFORM	:= $(shell uname)
 
-SAMPLESRC	:= 'int main(){return 0;}'
-EXECFLAGS	:= -c -o /dev/null - > /dev/null 2>&1
-HAVE_MULTILIB := $(shell \
-	(echo $(SAMPLESRC) | $(CC) -m32 -xc $(EXECFLAGS)) && \
-	(echo $(SAMPLESRC) | $(CXX) -m32 -xc++ $(EXECFLAGS)) && \
-	(echo $(SAMPLESRC) | $(CC) -m64 -xc $(EXECFLAGS)) && \
-	(echo $(SAMPLESRC) | $(CXX) -m64 -xc++ $(EXECFLAGS)); \
-	echo $$?)
-ifeq ($(HAVE_MULTILIB), 0)
-	MULTILIB	:= Yes
-endif
-
 ifneq (, $(findstring MINGW, $(PLATFORM)))
 	PLAT_WINNT	:= Yes
 endif
@@ -23,4 +11,22 @@ endif
 
 ifeq ($(PLATFORM), Darwin)
 	PLAT_DARWIN	:= Yes
+endif
+
+SAMPLESRC	:= 'int main(){return 0;}'
+
+ifdef PLAT_WINNT
+EXECFLAGS	:= -c -o /dev/null - > /dev/null 2>&1
+else
+EXECFLAGS	:= -o /dev/null - > /dev/null 2>&1
+endif
+
+HAVE_MULTILIB := $(shell \
+	(echo $(SAMPLESRC) | $(CC) -m32 -xc $(EXECFLAGS)) && \
+	(echo $(SAMPLESRC) | $(CXX) -m32 -xc++ $(EXECFLAGS)) && \
+	(echo $(SAMPLESRC) | $(CC) -m64 -xc $(EXECFLAGS)) && \
+	(echo $(SAMPLESRC) | $(CXX) -m64 -xc++ $(EXECFLAGS)); \
+	echo $$?)
+ifeq ($(HAVE_MULTILIB), 0)
+	MULTILIB	:= Yes
 endif
