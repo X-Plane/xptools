5491459e43d3918e2a31e4a1004b86d817944ee4
tgreene <theodorengreene@gmail.com>
2014-01-16 13:01:20 -0500

    Now that things are more undercontrol with the FSM this is some intitial work on it

--- msvc/xtools_wed/TaxiParser/ParserMain.cpp
+++ msvc/xtools_wed/TaxiParser/ParserMain.cpp
@@ -1,9 +1,6 @@
 #include "ParserMain.h"
-#include <stdio.h>
-#include <string>
-#include <conio.h>
-
 #define _CRT_SECURE_NO_WARNINGS
+
 ParserMain::ParserMain(void)
 {
 }
@@ -13,108 +10,269 @@ ParserMain::~ParserMain(void)
 {
 }
 
-static bool ValidateBasics(const char * inString)
+//Return if there was an error or not
+static bool ValidateBasics(InString * inStr)
 {
-	//What validation level you're out
-	int VAL_LEVEL = VLEV_WHITESPACE;
-	
-	//Original position
-	const char * oPos = inString;
-
-	//Moveable position
-	const char * nPos = oPos;
-
-	//End of the string (\0)
-	const char * endPos = strlen(inString) * sizeof(char) + nPos;
-	
-	int count = 0;
 	bool error = false;
 
 	//---White Space-----------------------------------------
 	printf("Checking to see if there is any whitespace...\n");
-	while(nPos != endPos)//Loop for the whole string
+	while(inStr->nPos != inStr->endPos)//Loop for the whole string
 	{
 		//If the current charecter is white space
-		if(isspace(*nPos))
+		if(isspace(*inStr->nPos))
 		{
-			error = true;
-			printf("FAIL: Char %d is whitespace.\n", count);
+			printf("Char %d is whitespace.\n", inStr->count);
+			return true;
 		}
 		//Increase the pointer and counter
-		nPos++;
-		count++;
+		inStr->nPos++;
+		inStr->count++;
 	}
-	if(error == false)
-	{
-		system ( "cls" );
-		printf("No whitespace errors detected. Moving on to ASCII Checking...\n");
-		VAL_LEVEL = VLEV_ISALLASCII;
-	}
-	else
-	{
-		return false;
-	}
+	printf("No whitespace errors detected. Moving on to ASCII Checking...\n");
 	//-------------------------------------------------------
 
-	//Reset variables
-	nPos=oPos;
-	count = 0;
-	error = false;
+	//Reset variable
+	inStr->ResetNPos();
 
 	//--ASCII------------------------------------------------
-	while(nPos != endPos)
-	{
-				
-		if( (33 >= (int) *nPos) || ((int) *nPos <= 126))
+	while(inStr->nPos != inStr->endPos)
+	{	
+		if( ((int) *inStr->nPos < 33 ) || ((int) *inStr->nPos > 126))
 		{
-			error = true;
-			printf("FAIL: Char %d is not valid ASCII. \n", count);
+			printf("Char at location %d is not valid ASCII. \n", inStr->nPos, inStr->count);
+			return true;
 		}
-		nPos++;
-		count++;
+		inStr->nPos++;
+		inStr->count++;
 	}
 	if(error == false)
 	{
-		system("cls");
 		printf("All ASCII valid\n");
 	}
-	else
+	//-------------------------------------------------------
+	inStr->ResetNPos();
+		
+	//end of the } brace pair, starts as the end of the string for safety
+	const char * curEnd = inStr->endPos;
+		
+	//What is currently considered good, a { a }
+	//We start by saying that we are looking for a {
+	char LFGoodMode = '{';//Used later for nesting nesting
+
+	//--Validate some basic curly brace rules
+	//1.) Must have atleast 1 pair
+	//2.) All { have a }
+	//3.) No pair may be empyt nest
+	//4.) No pair may nest
+
+	//--Find if and where the end of the pair is-----
+	//Run until it breaks on 1.)Finding }
+	//or reaching the end of the string
+	while(true)
 	{
-		return false;
+		//If you've found the other pair
+		if(*inStr->nPos == '}')
+		{
+			curEnd=inStr->nPos;
+			break;
+		}
+		if(inStr->nPos == inStr->endPos)
+		{
+			printf("You have no end to this pair starting from %d",inStr->count);
+			return true;
+		}
+		inStr->nPos++;
 	}
+	//---------------------------------------------
 
-	return true;
+	//Now that you are at done finding the bounds, reset
+	inStr->ResetNPos();
+
+	//--Next, find if it is actually empty---------
+	if(*(inStr->nPos+1) == '}')
+	{
+		printf("Empty curly braces detected!\n");
+		return true;
+	}
+	//---------------------------------------------
+			
+	//--Finally see if there is nesting------------
+
+	while(inStr->nPos != curEnd)
+	{
+		/* 1.)Decide whats good or bad
+		*		The first curly brace should be open
+		*		The second curly brace should be close
+		*		It should change never
+		* 2.)Test to see if it good or bad
+		*/
+
+		//If we are at a { or }
+		if((int)*inStr->nPos == '{' || (int) *inStr->nPos == '}')
+		{
+			//Is it the good mode?
+			if((int)*inStr->nPos == LFGoodMode)
+			{
+				//If so toggle what you are looking for
+				LFGoodMode = (*inStr->nPos == '{') ? '}' : '{';					
+			}
+			else
+			{
+				error = true;
+				printf("Char %c at location %d is invalid \n", *inStr->nPos,inStr->count);
+			}
+		}
+		inStr->count++;
+		inStr->nPos++;
+	}
+	//---------------------------------------------
+	inStr->ResetNPos();
+
+	return error;
 }
-static bool ValidateCurlyBraces(const char * inString)
+
+static bool FireAction(ACTIONS todo, char * optional=0)
 {
-	//Original position
-	const char * oPos = inString;
+	switch(todo)
+	{
+		case APPEND_ACCUMED:
+			break;
+		case CHANGE_COLOR:
+			break;
+		case TOGGLE_SIDE:
+			break;
+		case PRINT_MESSAGE:
+			break;
+		case THROW_ERROR:
+			break;
+		default:
+			return false;
+	}
+}
 
-	//Moveable position
-	const char * nPos = oPos;
+//Take in the current (and soon to be past) state  and the current letter being processed
+static FSM LookUpTable(FSM curState, char curChar)
+{
+	//If you have reached a \0 FOR ANY REASON exit now
+	if(curChar == '\0')
+	{
+		//FireAction(
+		return O_END;
+	}
+	switch(curState)
+	{
+	case I_IDLE:
+		switch(curChar)
+		{
+		}
+		break;
+	case I_INCUR:
+		switch(curChar)
+		{
+		case '@':
+			//FireAction(
+			return I_ANY_CONTROL;
+			break;
+		}
+		break;
+	case I_ACCUM_GLPHYS:	
+		switch(curChar)
+		{
+		}
 
-	//End of the string (\0)
-	const char * endPos = strlen(inString) * sizeof(char) + nPos;
-	
-	int count = 0;
-	bool error = false;
+		break;
+	case I_ANY_CONTROL:	
+		switch(curChar)
+		{
+		case 'Y':
+		case 'L':
+		case 'R':
+		case 'B':
+			//FireAction(Change color
+			return I_WAITING_SEPERATOR;
+			
+		case '@':
+			//FireAction(Change sides
+			return I_WAITING_SEPERATOR;
+		}
+		break;
+	case I_DOUBLE_CONTROL:	//This one may not be necissary
+		switch(curChar)
+		{
+
+		}
+
+		break;
+	case I_WAITING_SEPERATOR:	
+		switch(curChar)
+		{
+		}
+		break;
+	case O_ACCUM_GLYPHS:	
+		switch(curChar)
+		{
+		case '{':
+			return I_INCUR;
+			break;
+		}
+		break;
+	case O_END:	
+		switch(curChar)
+		{
+		}
+		break;
+	}
+	return I_IDLE;
 }
 
 int main(int argc, const char* argv[])
 {
-	printf("Welcome to the Taxi Sign Parser. \n Please input the string now \n");
+	printf("Welcome to the Taxi Sign Parser. \nPlease input the string now \n");
+	
+	//Take input and create and input string from it
 	char buf[1024];
 	scanf("%[^\n]",buf);
+	InString inStr(buf);
+
+	//Make the front and back outStrings
+	OutString outStr();
+
 	//Validate if there is any whitesapce or non printable ASCII charecters (33-126)
-	if(ValidateBasics(buf) == false)
+	if(1)
 	{
-		printf("String not valid");
-		return 0;
+		if(ValidateBasics(&inStr) == true)
+		{
+			printf("\n String not basically valid \n");
+			system("pause");
+			return 0;
+		}
+		system("cls");
 	}
-	if(ValidateCurlyBraces(buf) == false)
+	FSM FSM_MODE = O_ACCUM_GLYPHS;
+
+	while(FSM_MODE != O_END)
 	{
+		//Look up the transition
+		FSM transition = LookUpTable(FSM_MODE,*(inStr.nPos));
+		if(transition != LOOKUP_ERR)
+		{
+			FSM_MODE = transition;
+			inStr.nPos++;
+		}
+		else
+		{
+			printf("Fatal lookup error! State: %d, Char: %c, Location: %d\n",(int)FSM_MODE,*(inStr.nPos),inStr.count);
+			break;
+		}
 
 	}
 	system("pause");
 	return 0;
 }
+#if 0
+//Return if there was an error or not, validates only a set of them
+static bool ValidateCurlyBraces(InString * inStr)
+{
+}
+#endif
--- msvc/xtools_wed/TaxiParser/ParserMain.h
+++ msvc/xtools_wed/TaxiParser/ParserMain.h
@@ -1,14 +1,139 @@
 #pragma once
+#include <stdio.h>
+#include <string>
+#include <conio.h>
 
-enum ValidatorLevel
+enum FSM
 {
-	VLEV_WHITESPACE,
-	VLEV_ISALLASCII,
-	VLEV_ELEMENTS,
-	VLEV_CURLY,
-	VLEV_CURLY_INSIDE
-};
+	//The inside curly braces portion, starts with I_
+	I_IDLE,//For when we're not sure what will happen
+	I_INCUR,//For when we hit {
+	I_ACCUM_GLPHYS,//For collectings glpyhs
+	I_ANY_CONTROL,//When it hits a @
+	I_DOUBLE_CONTROL,//For when it hits a second @
+	I_WAITING_SEPERATOR,//For when it is waiting for a , or }
+
+	//The outside curlybraces portion, starts with O_
+	O_ACCUM_GLYPHS,
+	O_END,//For when the string ends
+	LOOKUP_ERR//Return code for any errors in the lookup table
 	
+};
+
+//Enum for actions to take
+enum ACTIONS
+{
+	APPEND_ACCUMED,
+	PREP_CHANGE_COLOR,
+	CHANGE_COLOR,
+	TOGGLE_SIDE,
+	PRINT_MESSAGE,
+	THROW_ERROR
+};
+
+struct InString
+{
+	//The original position
+	const char * oPos;
+	//The moving positino used to parse
+	const char * nPos;
+	//The end of string, strlen*char+oPos;
+	const char * endPos;
+	//Place count
+	int count;
+	InString::InString(const char * inString)
+	{
+		//Original position
+		oPos = inString;
+
+		//Moveable position
+		nPos = oPos;
+
+		//End of the string (\0)
+		endPos = strlen(inString) * sizeof(char) + oPos;
+		count = 0;
+	}
+	InString::~InString()
+	{
+
+	}
+
+	//Resets the NPos to where you want it to be,
+	//Called at the end of an operation on the string
+	//be careful when and where you reset the nPos!
+	void ResetNPos(const char * optionalOPos=NULL)
+	{
+		//Reset to the optionalOPos if there is one
+		if(optionalOPos != NULL)
+		{
+			nPos=optionalOPos;
+		}
+		else
+		{
+			nPos = oPos;//Otherwise put the nPos back to the
+			//original
+		}
+		count=0;
+	}
+};
+
+struct OutString
+{
+	//Front buffer
+	char oFBuf[1024];
+
+	//Back buffer
+	char oBBuf[1024];
+
+	//Place count (front/back)
+	int fCount;
+	int bCount;
+
+	OutString::OutString()
+	{
+		fCount = 0;
+		bCount = 0;
+	}
+	
+	OutString::~OutString()
+	{
+
+	}
+
+	//a letter to appened, front mode = 0, back mode = 1
+	void AppendLetter(char inLetter, int mode=0)
+	{
+		if(mode==0)
+		{
+			oFBuf[fCount] = inLetter;
+			fCount++;
+		}
+		else if(mode == 1)
+		{
+			oBBuf[bCount] = inLetter;
+			bCount++;
+		}
+		else
+		{
+			printf("You have put in a bad mode!");
+		}
+	}
+
+	void PrintString()
+	{
+		for (int i = 0; i <= fCount; i++)
+		{
+			printf("%c",oFBuf[fCount]);
+		}
+
+		printf("\n");
+		for (int i = 0; i <= bCount; i++)
+		{
+			printf("%c",oFBuf[fCount]);
+		}
+	}
+};
+
 class ParserMain
 {
 public:
