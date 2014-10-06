bb8e57dab67625dcb6cc205ef71799b46d73387f
tgreene <theodorengreene@gmail.com>
2014-10-06 17:49:41 +1300

    Adds some much needed code clean up to this mess and adds more commenting

--- src/WEDImportExport/WED_GatewayImport.cpp
+++ src/WEDImportExport/WED_GatewayImport.cpp
@@ -73,9 +73,9 @@
 
 enum imp_dialog_stages
 {
-imp_dialog_choose_ICAO,//if !GET airports list, GET. Let user pick ICAO from ICAO table
-imp_dialog_choose_versions,//GET airport:scenery pack array. Let user pick scenery pack(s) to download
-imp_dialog_download_version,//Download scenery pack, save the contents in the right place, import to WED
+imp_dialog_choose_ICAO,//Let the user choose the aiport from the table. The required GET is done before hand
+imp_dialog_choose_versions,//Let user pick scenery pack(s) to download. The required GET is done before hand
+imp_dialog_download_specific_version,//Download scenery pack, save the contents in the right place, import to WED
 imp_dialog_finish//Clean up any processes left
 };
 
@@ -93,6 +93,7 @@ public:
 	WED_GatewayImportDialog(WED_Document * resolver, GUI_Commander * cmdr);
 	~WED_GatewayImportDialog();
 
+private:
 	//Fired when selecting the next button
 	void Next();
 
@@ -105,7 +106,7 @@ public:
 							GUI_Broadcaster *		inSrc,
 							intptr_t    			inMsg,
 							intptr_t				inParam);
-private:
+
 #if DEV
 	friend class WED_AppMain;
 #endif
@@ -139,15 +140,16 @@ private:
 	GUI_TextTable			mICAO_TextTable;
 	GUI_TextTableHeader		mICAO_TextTableHeader;
 	
+	//Starts the download of the json file containing the airports list
+	void StartICAODownload();
+	//From the downloaded JSON, fill the ICAO table
+	void FillICAOFromJSON();
 		//--ICAO Table Provider/Geometry
 		WED_AptTable			mICAO_AptProvider;
 		AptVector				mICAO_Apts;
 
-		//Extracts the Code from the constructor
+		//Sets up the ICAO table GUI and event handlers
 		void MakeICAOTable(int bounds[4]);
-		
-		//Starts too much downloads the json file specified by current stage of the program
-		void StartICAODownload();
 		//----------------------//
 	//----------------------//
 
@@ -160,16 +162,20 @@ private:
 	GUI_TextTable			mVersions_TextTable;
 	GUI_TextTableHeader		mVersions_TextTableHeader;
 	
+	//Starts the download of the json file to populate the versions table
+	void StartVersionsDownload();
+
+	//From the downloaded JSON, fill the versions table
+	void FillVersionsFromJSON();
+	//Starts the download of the specific version, aiming to get the masterZipBlob contained
+	void StartSpecificVersionDownload(int id);
+
 		//--Versions Table Provider/Geometry
 		WED_VerTable			mVersions_VerProvider;
 		VerVector				mVersions_Vers;
 
-		//Extracts the Code from the constructor
+		//Sets up the Versions table GUI and event handlers
 		void MakeVersionsTable(int bounds[4]);
-
-		//Downloads the json file specified by current stage of the program
-		void StartVersionsDownload();
-		void StartVersionDownload(int id);
 		//---------------------//
 	//--------------------------//
 		
@@ -196,7 +202,7 @@ WED_GatewayImportDialog::WED_GatewayImportDialog(WED_Document * resolver, GUI_Co
 	mVersions_VerProvider(&mVersions_Vers),
 	mVersions_TextTable(this,100,0)
 {
-	resolver->AddListener(this);
+	mResolver->AddListener(this);
 
 	//mPacker
 	int bounds[4];
@@ -207,8 +213,6 @@ WED_GatewayImportDialog::WED_GatewayImportDialog(WED_Document * resolver, GUI_Co
 	GUI_Pane::GetBounds(bounds);
 	mPacker->SetBounds(bounds);
 	mPacker->SetBkgkndImage ("gradient.png");
-
-	
 
 	//Filter
 	mFilter = new WED_FilterBar(this,filter_changed,0,"Filter:","",NULL,false);
@@ -280,6 +284,11 @@ WED_GatewayImportDialog::WED_GatewayImportDialog(WED_Document * resolver, GUI_Co
 
 WED_GatewayImportDialog::~WED_GatewayImportDialog()
 {
+	if(mCurl != NULL)
+	{
+		delete mCurl;
+		mCurl = NULL;
+	}
 }
 
 void WED_GatewayImportDialog::DoAirportImport(string filePath)
@@ -309,11 +318,10 @@ void WED_GatewayImportDialog::Next()
 		mVersions_Scroller->Show();
 		mVersions_Header->Show();*/
 		break;
-	
 	case imp_dialog_choose_versions:
-		StartVersionDownload(mVersions_Vers[0].sceneryId);
+		StartSpecificVersionDownload(mVersions_Vers[0].sceneryId);
 		break;
-	case imp_dialog_download_version:
+	case imp_dialog_download_specific_version:
 		break;
 	case imp_dialog_finish:
 
@@ -356,90 +364,13 @@ void WED_GatewayImportDialog::TimerFired()
 		{
 			if(mPhase == imp_dialog_choose_ICAO)
 			{
-				//create a string from the vector of chars
-				string rawJSONString = string(rawJSONBuf.begin(),rawJSONBuf.end());
-
-				Json::Value root;
-				Json::Reader reader;
-				bool success = reader.parse(rawJSONString,root);
-	
-				//Check for errors
-				if(success == false)
-				{
-					DoUserAlert("Airports list is invalid data, ending dialog box");
-					this->AsyncDestroy();
-					return;
-				}
-
-				Json::Value mAirportsGET = Json::Value(Json::arrayValue);
-				mAirportsGET.swap(root["airports"]);
-
-				//loop through the whole array
-				for (int i = 0; i < mAirportsGET.size(); i++)
-				{
-					//Get the current scenery object
-					Json::Value tmp(Json::objectValue);
-					tmp = mAirportsGET.operator[](i);//Yes, you need the verbose operator[] form, yes it's dumb
-
-					AptInfo_t cur_airport;
-					cur_airport.icao = tmp["AirportCode"].asString();
-					cur_airport.name = tmp["AirportName"].asString();
-
-					//Add the current scenery object's airport code
-					mICAO_Apts.push_back(cur_airport);
-				}
-				mICAO_AptProvider.AptVectorChanged();
-			}
+				FillICAOFromJSON();
+			}		
 			else if(mPhase == imp_dialog_choose_versions)
 			{
-				//create a string from the vector of chars
-				string rawJSONString = string(rawJSONBuf.begin(),rawJSONBuf.end());
-
-				//Now that we have our rawJSONString we'll be turning it into a JSON object
-				Json::Value root(Json::objectValue);
-	
-				Json::Reader reader;
-				bool success = reader.parse(rawJSONString,root);
-	
-				//Check for errors
-				if(success == false)
-				{
-					DoUserAlert("Airports list is invalid data, ending dialog box");
-					this->AsyncDestroy();
-					return;
-				}
-				Json::Value airport = root["airport"];
-				Json::Value sceneryArray = Json::Value(Json::arrayValue);
-				sceneryArray = airport["scenery"];
-
-				//Build up the table
-				for (Json::ValueIterator itr = sceneryArray.begin(); itr != sceneryArray.end(); itr++)
-				{
-					//TODO - actually access the scenery pack, you are currently accessing the array of scenery packs
-					Json::Value curScenery = *itr;
-					VerInfo_t tmp;
-					
-					tmp.sceneryId = curScenery.operator[]("sceneryId").asInt();
-					tmp.parentId = curScenery.operator[]("parentId").asInt();
-					tmp.userId = curScenery.operator[]("userId").asInt();
-					tmp.userName = curScenery.operator[]("userName").asString();
-					//Dates will appear as ISO8601: https://en.wikipedia.org/wiki/ISO_8601
-					//For example 2014-07-31T14:34:47.000Z
-					tmp.dateUploaded = curScenery.operator[]("dateUpload").asString();
-					tmp.dateAccepted = curScenery.operator[]("dateAccepted").asString();
-					tmp.dateApproved = curScenery.operator[]("dateApproved").asString();
-					//2 for 2D =  3 for 3D
-					tmp.type = curScenery.operator[]("type").asString();
-					//TODO-tmp.features = curScenery.operator[]("features").asString();
-					tmp.artistComments = curScenery.operator[]("artistComments").asString();
-					tmp.moderatorComments = curScenery.operator[]("moderatorComments").asString();
-					
-					mVersions_Vers.push_back(tmp);
-				}
-				mVersions_VerProvider.VerVectorChanged();				
-				return;
+				FillVersionsFromJSON();
 			}
-			else if(mPhase == imp_dialog_download_version)
+			else if(mPhase == imp_dialog_download_specific_version)
 			{
 				//create a string from the vector of chars
 				
@@ -449,12 +380,19 @@ void WED_GatewayImportDialog::TimerFired()
 				
 				Json::Value root = Json::Value(Json::objectValue);
 				Json::Reader reader;
-				bool was_success = reader.parse(rawJSONString,root);
+				bool success = reader.parse(rawJSONString,root);
+
+				//Check for errors
+				if(success == false)
+				{
+					DoUserAlert("Airports list is invalid data, ending dialog box");
+					this->AsyncDestroy();
+					return;
+				}
 
 				string zipString = root["scenery"]["masterZipBlob"].asString();
 				vector<char> outString = vector<char>(zipString.length());
 
-				
 				char * outP;
 				decode(&*zipString.begin(),&*zipString.end(),&*outString.begin(),&outP);
 
@@ -479,6 +417,7 @@ void WED_GatewayImportDialog::TimerFired()
 
 				fclose(f);
 
+				//--This is where the MemFilesAPI will head into
 				int breakhereandmanuallyunzip = 0;
 				
 				WED_Thing * wrl = WED_GetWorld(mResolver);
@@ -500,11 +439,103 @@ void WED_GatewayImportDialog::TimerFired()
 				string dsf_txt_path = filePath.substr(0,filePath.size()-4) + ".txt";
 				DSF_Import(dsf_txt_path.c_str(), (WED_Group *) g);
 				wrl->CommitOperation();
-			}//end if(mPhase == imp_dialog_download_version
+				
+				//Set it back so we can download another
+				mPhase = imp_dialog_choose_versions;
+			}//end if(mPhase == imp_dialog_download_specific_version
 		}//end if(mCurl->is_ok())
+		else if(mCurl->get_error() <= CURL_LAST)//start our error checking process
+		{
+			
+		}
 	}//end if(mCurl->is_done())
 }//end WED_GatewayImportDialog::TimerFired()
 
+void WED_GatewayImportDialog::FillICAOFromJSON()
+{
+	//create a string from the vector of chars
+	string rawJSONString = string(rawJSONBuf.begin(),rawJSONBuf.end());
+
+	Json::Value root;
+	Json::Reader reader;
+	bool success = reader.parse(rawJSONString,root);
+	
+	//Check for errors
+	if(success == false)
+	{
+		DoUserAlert("Airports list is invalid data, ending dialog box");
+		this->AsyncDestroy();
+		return;
+	}
+
+	Json::Value mAirportsGET = Json::Value(Json::arrayValue);
+	mAirportsGET.swap(root["airports"]);
+
+	//loop through the whole array
+	for (int i = 0; i < mAirportsGET.size(); i++)
+	{
+		//Get the current scenery object
+		Json::Value tmp(Json::objectValue);
+		tmp = mAirportsGET.operator[](i);//Yes, you need the verbose operator[] form. Yes it's dumb
+
+		AptInfo_t cur_airport;
+		cur_airport.icao = tmp["AirportCode"].asString();
+		cur_airport.name = tmp["AirportName"].asString();
+
+		//Add the current scenery object's airport code
+		mICAO_Apts.push_back(cur_airport);
+	}
+	mICAO_AptProvider.AptVectorChanged();
+}
+
+void WED_GatewayImportDialog::FillVersionsFromJSON()
+{
+	//create a string from the vector of chars
+	string rawJSONString = string(rawJSONBuf.begin(),rawJSONBuf.end());
+
+	//Now that we have our rawJSONString we'll be turning it into a JSON object
+	Json::Value root(Json::objectValue);
+	
+	Json::Reader reader;
+	bool success = reader.parse(rawJSONString,root);
+	
+	//Check for errors
+	if(success == false)
+	{
+		DoUserAlert("Scenery list is invalid data, ending dialog box");
+		this->AsyncDestroy();
+		return;
+	}
+	Json::Value airport = root["airport"];
+	Json::Value sceneryArray = Json::Value(Json::arrayValue);
+	sceneryArray = airport["scenery"];
+
+	//Build up the table
+	for (Json::ValueIterator itr = sceneryArray.begin(); itr != sceneryArray.end(); itr++)
+	{
+		//TODO - actually access the scenery pack, you are currently accessing the array of scenery packs
+		Json::Value curScenery = *itr;
+		VerInfo_t tmp;
+					
+		tmp.sceneryId = curScenery.operator[]("sceneryId").asInt();
+		tmp.parentId = curScenery.operator[]("parentId").asInt();
+		tmp.userId = curScenery.operator[]("userId").asInt();
+		tmp.userName = curScenery.operator[]("userName").asString();
+		//Dates will appear as ISO8601: https://en.wikipedia.org/wiki/ISO_8601
+		//For example 2014-07-31T14:34:47.000Z
+		tmp.dateUploaded = curScenery.operator[]("dateUpload").asString();
+		tmp.dateAccepted = curScenery.operator[]("dateAccepted").asString();
+		tmp.dateApproved = curScenery.operator[]("dateApproved").asString();
+		//2 for 2D =  3 for 3D
+		tmp.type = curScenery.operator[]("type").asString();
+		//TODO-tmp.features = curScenery.operator[]("features").asString();
+		tmp.artistComments = curScenery.operator[]("artistComments").asString();
+		tmp.moderatorComments = curScenery.operator[]("moderatorComments").asString();
+					
+		mVersions_Vers.push_back(tmp);
+	}
+	mVersions_VerProvider.VerVectorChanged();
+}
 void WED_GatewayImportDialog::ReceiveMessage(
 							GUI_Broadcaster *		inSrc,
 							intptr_t    			inMsg,
@@ -583,7 +614,7 @@ void WED_GatewayImportDialog::StartVersionsDownload()
 	Start(1.0);
 }
 
-void WED_GatewayImportDialog::StartVersionDownload(int id)
+void WED_GatewayImportDialog::StartSpecificVersionDownload(int id)
 {
 	//a buffer of chars to be filled, reserve a huge amount of space for it cause we'll need it
 	rawJSONBuf = vector<char>(VERSION_GET_SIZE_GUESS);
@@ -606,6 +637,7 @@ void WED_GatewayImportDialog::StartVersionDownload(int id)
 	mCurl = new curl_http_get_file(url,&rawJSONBuf,cert);
 	Start(1.0);
 }
+
 void WED_GatewayImportDialog::MakeICAOTable(int bounds[4])
 {
 	mICAO_AptProvider.SetFilter(mFilter->GetText());//This requires mApts to be full
@@ -675,7 +707,6 @@ void WED_GatewayImportDialog::MakeICAOTable(int bounds[4])
 	mICAO_Scroller->PositionHeaderPane(mICAO_Header);
 }
 
-//Extracts the Code from the constructor
 void WED_GatewayImportDialog::MakeVersionsTable(int bounds[4])
 {
 	mVersions_VerProvider.SetFilter(mFilter->GetText());//This requires mVers to be full?
@@ -748,8 +779,6 @@ void WED_GatewayImportDialog::MakeVersionsTable(int bounds[4])
 	mPacker->PackPane(mVersions_Scroller,gui_Pack_Center);
 	mVersions_Scroller->PositionHeaderPane(mVersions_Header);
 }
-
-
 //-------------------------------------------------------------
 
 int	WED_CanImportFromGateway(IResolver * resolver)
