793ebc443dfb52aca634057834edce0e0cd14dc8
bsupnik <bsupnik@xsquawkbox.net>
2015-03-30 19:02:58 -0400

    lots of version bumps for scenery tools 15-3 and MT 3.0b1

--- src/XPTools/version.h
+++ src/XPTools/version.h
@@ -5,19 +5,19 @@
 #include <string.h>
 #include <stdint.h>
 
-#define XPT_PACKAGE_VER		"12-2"
+#define XPT_PACKAGE_VER		"15-3"
 
-#define DDSTOOL_VER		10200
+#define DDSTOOL_VER		10300
 #define DDSTOOL_EXTRAVER	"-r1"
 
-#define DSFTOOL_VER		20000
+#define DSFTOOL_VER		20100
 #define DSFTOOL_EXTRAVER	"-r1"
 
 #define OBJCONVERT_VER		30000
 #define OBJCONVERT_EXTRAVER	"-r1"
 
-#define MESHTOOL_VER		20000
-#define MESHTOOL_EXTRAVER	"-r2"
+#define MESHTOOL_VER		30000
+#define MESHTOOL_EXTRAVER	"-b1"
 
 #define AC3DPLUGIN_VER		30201
 #define AC3DPLUGIN_EXTRAVER	"-r2"
@@ -39,7 +39,7 @@ product_version(uint32_t ver, const char* extra_ver)
 static inline void
 print_product_version(const char* product, uint32_t ver, const char* extra_ver)
 {
-	printf("%s %s, Copyright 2009 Laminar Research.  Compiled on "
+	printf("%s %s, Copyright 2015 Laminar Research.  Compiled on "
 		__DATE__".\n", product,	product_version(ver, extra_ver));
 	printf("Part of X-Plane Scenery Tools release: %s\n", XPT_PACKAGE_VER);
 }
