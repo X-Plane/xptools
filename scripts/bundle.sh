bb036c61925e038b4f478aab8a605422bc47e38f
bsupnik <bsupnik@xsquawkbox.net>
2015-04-01 15:53:40 -0400

    WED readme added for release notes bundle script updated for code signing and windows

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# usage: bundle.sh product platform version
+# usage: ./bundle.sh [--codesign <signature>] ac3d|meshtool|tools|wed win|mac|lin version
+
+want_sign=0
+
+if [ $1 == "--codesign" ]; then
+	want_sign=1
+	sig_id=$2
+	shift
+	shift
+fi
 
 cd ..
 home_dir=`pwd`
@@ -26,10 +35,14 @@ mac)
 	asuffix=".app"
 	;;
 win)
-	build_dir=build/Mingw/release_opt
+	build_dir=build
+	rm -rf $build_dir
 	suffix=Win
 	tsuffix=".exe"
 	asuffix=".exe"
+	mkdir $build_dir
+	find msvc -path \*/Release/\*.exe -exec cp '{}' $build_dir \;
+	mv $build_dir/WorldEditor.exe $build_dir/WED.exe 
 	;;
 lin)
 	build_dir=build/Linux/release_opt
@@ -63,22 +76,29 @@ meshtool)
 wed)
 	out_file="wed_"$2"_"$3".zip"
 	src_files="WED$asuffix"
-	#doc_files="src/WEDResources/WEDManual.pdf"
-	doc_files=""
+	doc_files="src/WEDCore/README.WorldEditor"
 	;;
 tools)
 	echo $build_dir/tools
 	mkdir $build_dir/tools
-	mv $build_dir/ObjConverter$tsuffix $build_dir/DDSTool$tsuffix $build_dir/DSFTool$tsuffix $build_dir/tools
+	mv $build_dir/DDSTool$tsuffix $build_dir/DSFTool$tsuffix $build_dir/tools
 	out_file="xptools_"$2"_"$3".zip"
-	src_files="tools/ObjConverter$tsuffix tools/DDSTool$tsuffix tools/DSFTool$tsuffix ObjView$asuffix XGrinder$asuffix"
-	doc_files="src/XPTools/README_FIRST src/XPTools/README.DDSTool src/XPTools/README.ObjConverter src/XPTools/README.ObjView src/XPTools/README.XGrinder"
+	src_files="tools/DDSTool$tsuffix tools/DSFTool$tsuffix ObjView$asuffix XGrinder$asuffix"
+	doc_files="src/XPTools/README_FIRST src/XPTools/README.DDSTool src/XPTools/README.ObjView src/XPTools/README.XGrinder"
 	;;
 esac
 
 cd "$home_dir/scripts"
 rm $out_file
 cd ../$build_dir
+
+if [ $want_sign == 1 ]; then
+	for f in $src_files 
+	do
+	   codesign --sign $sig_id $f
+	done
+fi
+
 zip -r "$home_dir/scripts/$out_file" $src_files
 cd "$home_dir"
 zip -j "$home_dir/scripts/$out_file" $doc_files
