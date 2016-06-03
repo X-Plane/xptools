8f0dca86271982ed1bcfdda382870ec2f1c072b0
tngreene <theodorengreene@gmail.com>
2016-06-03 12:42:36 -0400

    Fixes the undo being out of step. Also fixes the undo text from displaying "Undo Add Metadata enum_key_name". This replaces it with nice looking text.

--- src/WEDWindows/WED_GroupCommands.cpp
+++ src/WEDWindows/WED_GroupCommands.cpp
@@ -408,8 +408,8 @@ void WED_DoAddMetaData(IResolver * inResolver, const string& key)
 	WED_Airport * want_sel = SAFE_CAST(WED_Airport, sel->GetNthSelection(0));
 	if (want_sel == NULL) return;
 	want_sel->StartOperation(string("Add Metadata Key " + key).c_str());
-	want_sel->AddMetaDataKey(key, "");
 	want_sel->StateChanged();
+	want_sel->AddMetaDataKey(key, "");
 	want_sel->CommitOperation();
 }
 
