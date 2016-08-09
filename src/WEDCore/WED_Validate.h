48af7bbb556f1c97a8aaf7adfa41686368783081
Benjamin Supnik <bsupnik@xsquawkbox.net>
2016-08-08 23:08:04 -0400

    Huge restructure of the validation code - validation errors are now returned as a data type for more thorough validation and better code structure.  All globals also removed.
    This also adds validation to catch opposite direction arrivals.
    Fixes WED-585

--- src/WEDCore/WED_Validate.h
+++ src/WEDCore/WED_Validate.h
@@ -26,7 +26,48 @@
 
 class	IResolver;
 class	WED_Thing;
+class	WED_Airport;
+
+// The validation error record stores a single validation problem for reporting.
+// This consists of:
+// - One error message loosely describing what went wrong and
+// - One or more objects participating in the problem.  At least one object is mandatory.
+// - If the objects are within an airport, the parent airport that is now effectively invalid/unexportable.
+
+struct	validation_error_t {
+
+	validation_error_t() : airport(NULL) { }
+	
+	// This constructor creates a validation error with a single object ("who") participating.  Due to C++ weirdness
+	// we have to template; the assumption is that "who" is a WED_Thing derivative.
+	template <typename T>
+	validation_error_t(const string& m, T * who, WED_Airport * a) : msg(m), airport(a) { bad_objects.push_back(who); }
+
+	// This constructor takes an arbitrary container of ptrs to WED_Thing derivatives and builds a single validation
+	// failure with every object listed.
+	template <typename T>
+	validation_error_t(const string& msg, const T& container, WED_Airport * airport);
+	
+
+	WED_Airport *		airport;		// NULL if error is in an object outside ANY airport
+	vector<WED_Thing *>	bad_objects;	// object(s) involved in the validation error - at least one required.
+	string				msg;
+
+};
 
 bool	WED_ValidateApt(IResolver * resolver, WED_Thing * root = NULL);	// if root not null, only do this sub-tree
+
+
+
+
+
+template <typename T>
+validation_error_t::validation_error_t(const string& m, const T& container, WED_Airport * a) :
+	msg(m), airport(a)
+{
+	copy(container.begin(), container.end(), back_inserter(bad_objects));
+}
+
+
 
 #endif
