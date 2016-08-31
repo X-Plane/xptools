f6281029acb41a2ac44d4988e1644d4fc1534868
Ben Supnik <ben@x-plane.com>
2016-08-31 09:19:56 -0400

    version bump - wed151b2

--- src/WEDCore/WED_Version.h
+++ src/WEDCore/WED_Version.h
@@ -28,13 +28,13 @@
 // so you can't go using const int and other fancy-pants C++ stuff!
 
 // These versions are used in about boxes, resources, info boxes, etc.
-#define	WED_VERSION				1.5.0b1
-#define	WED_VERSION_STRING		"1.5.0b1"
+#define	WED_VERSION				1.5.0b2
+#define	WED_VERSION_STRING		"1.5.0b2"
 #define	WED_VERSION_STRING_SHORT	"1.5.0"		// omit beta  number
-#define	WED_VERSION_RES			"1.5.0b1"
-#define	WED_VERSION_BIN			1,5,0,0
+#define	WED_VERSION_RES			"1.5.0b2"
+#define	WED_VERSION_BIN			1,5,0,1
 
 // This numeric is used by the gateway to understand if our WED is up-to-date.
-#define WED_VERSION_NUMERIC		15000
+#define WED_VERSION_NUMERIC		15001
 
 #endif /* WED_Version_H */
