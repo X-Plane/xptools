866a1635123b5feb491ccd5096c5123456c558dc
Ben Supnik <ben@x-plane.com>
2016-10-10 21:31:19 -0400

    fixed case in undo message

--- src/WEDMap/WED_VertexTool.cpp
+++ src/WEDMap/WED_VertexTool.cpp
@@ -113,7 +113,7 @@ void	WED_VertexTool::BeginEdit(void)
 	ISelection * sel = WED_GetSelect(GetResolver());
 	IOperation * op = dynamic_cast<IOperation *>(sel);
 	DebugAssert(sel != NULL && op != NULL);
-	op->StartOperation("Vertex modification");  // can be any of - split ATC edge - move ATC edge node
+	op->StartOperation("Vertex Modification");  // can be any of - split ATC edge - move ATC edge node
 	                                 // - drag a node or modify a bezier node of any previously selected feature
 }
 
