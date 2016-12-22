6c76e311f61cb077c1e298ca0fe93fa06d692758
Ben Supnik <ben@x-plane.com>
2016-12-22 09:41:14 -0500

    bug fix: fix crash when undoing edge creation We need to defer entity geo-cache invalidation for edges, because the invalidation may need to walk pointer links that aren't established mid-archive rebuild.
    This commit introduces a new callback when the archive is restored from undo - after all objects are restored,
    requesting objects get a one-time callback to
    
    fixes WED-728

--- src/WEDEntities/WED_Select.h
+++ src/WEDEntities/WED_Select.h
@@ -63,7 +63,7 @@ public:
 	virtual		int				IterateSelectionAnd(int (* func)(ISelectable * who, void * ref), void * ref) const;
 
 	// WED_Persistent
-	virtual		void 			ReadFrom(IOReader * reader);
+	virtual		bool 			ReadFrom(IOReader * reader);
 	virtual		void 			WriteTo(IOWriter * writer);
 	virtual		void			FromDB(sqlite3 * db, const map<int,int>& mapping);
 	virtual		void			ToDB(sqlite3 * db);
