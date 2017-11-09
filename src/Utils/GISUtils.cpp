bb1cd492f4f86c562e0c3f6ee026c6e67fa9c9b5
meikel <michael.minnhaar@yahoo.com>
2017-11-08 21:40:44 -0800

    compile fixes for libgeotiff-1.4.2 under windows

--- src/Utils/GISUtils.cpp
+++ src/Utils/GISUtils.cpp
@@ -40,12 +40,11 @@
 // set to 1 to save geotiff inside geojp2 to disk
 #define DUMP_GTIF 0
 
-#if defined(IBM)
-	#include <libxtiff/xtiffio.h>
+#if IBM
 	#include "GUI_Unicode.h"
-#else
-	#include <xtiffio.h>
 #endif
+#include <xtiffio.h>
+
 void	make_cache_file_path(const char * cache_base, int west, int south, const char * cache_name, char path[1024])
 {
 	sprintf(path, "%s%s%+03d%+04d%s%+03d%+04d.%s.txt", cache_base, DIR_STR, latlon_bucket (south), latlon_bucket (west), DIR_STR, (int) south, (int) west, cache_name);
