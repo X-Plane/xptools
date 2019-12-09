d73d1fb4cb61eff41d7a85c1a800fcabc8677c65
meikelm <michael.minnhaar@yahoo.com>
2019-12-09 14:43:13 -0800

    update msvc project file

--- src/Obj/XDefs.h
+++ src/Obj/XDefs.h
@@ -90,6 +90,8 @@
 // This enablse curved ATC taxiways - feature is NOT done yet or offical so, like, don't use it.
 #define HAS_CURVED_ATC_ROUTE 0
 
+#define USE_7Z 1
+
 // This is a big hack.  WED objects have culling "built-in" based on a bounding rect - it's part of the IGIS interface.
 // But this is kind of a design flaw; the actual culling depends on the -visualization-, which is applied via a map layer.
 // Only the map visualization knows how big things are.
