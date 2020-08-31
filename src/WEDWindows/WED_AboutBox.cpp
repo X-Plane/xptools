1ef55f8f1ad940b61aa8e74c1fe1e6a96ea96919
meikelm <michael.minnhaar@yahoo.com>
2020-08-31 13:16:48 -0700

    Add FLTK notice to about box (linux only)

--- src/WEDWindows/WED_AboutBox.cpp
+++ src/WEDWindows/WED_AboutBox.cpp
@@ -30,7 +30,7 @@
 #include "BitmapUtils.h"
 #include "WED_Version.h"
 
-static int aboutbox_bounds[4] = { 0, 0, 512, 384};
+static int aboutbox_bounds[4] = { 0, 0, 600, 420};
 
 WED_AboutBox::WED_AboutBox(GUI_Commander * cmdr) : GUI_Window("About WED", xwin_style_movable | xwin_style_centered | xwin_style_popup, aboutbox_bounds, cmdr)
 {
@@ -61,25 +61,29 @@ void		WED_AboutBox::Draw(GUI_GraphState * state)
 
 	const char * main_text[] = {
 		"WorldEditor " WED_VERSION_STRING_SHORT,
-		"© Copyright 2007-2019, Laminar Research",
+		"© Copyright 2007-2020, Laminar Research",
 		"",
-		"This software is available under an open",
-		"license. For more info visit",
-		"http://developer.x-plane.com/code/",
+		"This software is available under an open license,",
+		"for more info see http://developer.x-plane.com/code",
 		"",
-		"OSM data available under CC-BY-SA 2.0",
-		"license by the OpenStreetMap foundation",
+		"OSM data available under CC-BY-SA 2.0 license",
+		"from the OpenStreetMap foundation",
 		"http://www.openstreetmap.org/copyright",
 		"",
-		"The World Imagery is provided by ESRI",
-		"For more detail see 'Permitted Uses' in the",
-		"Help menu or visit http://www.esri.com",
+		"The World Imagery is provided by ESRI, for",
+		"more detail see 'Permitted Uses' in the Help",
+		"menu or visit http://www.esri.com",
+#if LIN
+		"",
+		"WED is based in part on the work of the FLTK",
+		"project hosted at http://www.fltk.org",
+#endif
 		0 };
 
 	int n = 0;
 	while(main_text[n])
 	{
-		GUI_FontDraw(state, font, color, bounds[0] * 0.6 + bounds[2] * 0.4, bounds[3] - 30 - f*n, main_text[n]);
+		GUI_FontDraw(state, font, color, bounds[0] * 0.62 + bounds[2] * 0.38, bounds[3] - 30 - f*n, main_text[n]);
 		++n;
 	}
 
@@ -87,7 +91,7 @@ void		WED_AboutBox::Draw(GUI_GraphState * state)
 
 	GUI_FontDraw(state, font, color,
 		(bounds[0] + bounds[2]) * 0.5,
-		bounds[1] * 0.7 + bounds[3] * 0.3,
+		bounds[1] * 0.75 + bounds[3] * 0.25,
 		info, align_Center);
 
 	// Don't like how you have been acredited (or NOT acredited)?  FIX IT!  Change the code
@@ -105,7 +109,7 @@ void		WED_AboutBox::Draw(GUI_GraphState * state)
 		GUI_FontDraw(
 			state,font,color,
 			(bounds[0] + bounds[2]) * 0.5,
-			bounds[1] * 0.8 + bounds[3] * 0.2 - n * f,
+			bounds[1] * 0.85 + bounds[3] * 0.15 - n * f,
 			credits[n],align_Center);
 		++n;
 	}
