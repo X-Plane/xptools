X-Plane/xptools

commit fdd8ad1547e608f952e121648c9a72181b72204c

add placeholders for future toolicons

src/WEDMap/WED_MapPane.cpp
@@ -64,7 +64,9 @@
 #endif
 #include "WED_SlippyMap.h"
 
-char	kToolKeys[] = {
+#define TOOLICON_ROWS 15
+
+char	kToolKeys[2*TOOLICON_ROWS] = {
 	0, 0,
 	0, 0, 0, 0,
 	0, 0, 0, 0,
@@ -73,7 +75,8 @@ char	kToolKeys[] = {
 	'e', 'w', 'f', 'g',
 
 	'l', 'k', 'h', 't',
-	'r', 's', 'v', 'm'
+	'r', 's', 'v', 'm',
+	0, 0, 0, 0
 };
 
 enum //Must be kept in sync with TabPane
@@ -163,6 +166,11 @@ WED_MapPane::WED_MapPane(GUI_Commander * cmdr, double map_bounds[4], IResolver *
 	}
 #endif
 	// TOOLS
+	mTools.push_back(					NULL);
+	mTools.push_back(					NULL);
+
+	mTools.push_back(					NULL); // icon for .ags/.agb placement
+	mTools.push_back(					NULL); // icon for JetWay placement
 
 	mTools.push_back(					new WED_CreatePointTool("Truck Parking", mMap, mMap, resolver, archive, create_TruckParking));
 	mTools.push_back(					new WED_CreatePointTool("Truck Destination", mMap, mMap, resolver, archive, create_TruckDestination));
@@ -208,6 +216,8 @@ WED_MapPane::WED_MapPane(GUI_Commander * cmdr, double map_bounds[4], IResolver *
 	mTools.push_back(					new WED_VertexTool("Vertex",mMap, mMap, resolver, 1));
 	mTools.push_back(					new WED_MarqueeTool("Marquee",mMap, mMap, resolver));
 
+	Assert(mTools.size() == 2 * TOOLICON_ROWS);
+
 	mInfoAdapter = new WED_ToolInfoAdapter(GUI_GetImageResourceHeight("property_bar.png") / 2);
 	mTextTable = new GUI_TextTable(cmdr,12,0);
 	mTable = new GUI_Table(1);
@@ -236,7 +246,7 @@ WED_MapPane::WED_MapPane(GUI_Commander * cmdr, double map_bounds[4], IResolver *
 
 	mInfoAdapter->AddListener(mTable);
 
-	mToolbar = new GUI_ToolBar(2,13,"map_tools.png");
+	mToolbar = new GUI_ToolBar(2,TOOLICON_ROWS,"map_tools.png");
 	mToolbar->SizeToBitmap();
 	mToolbar->Show();
 	mToolbar->SetParent(this);
@@ -723,7 +733,7 @@ void hide_all_persistents(MapFilter_t& hide_list)
 	hide_list.push_back(WED_ATCRunwayUse::sClass);
 	hide_list.push_back(WED_TruckDestination::sClass);
 	hide_list.push_back(WED_TruckParkingLocation::sClass);
-	
+
 #if ROAD_EDITING
 	hide_list.push_back(WED_RoadEdge::sClass);
 #endif // ROAD_EDITING

makerules/WED
@@ -379,7 +379,6 @@ RESOURCES += ./src/WEDResources/scroll_btn_h.png
 RESOURCES += ./src/WEDResources/scroll_btn_v.png
 RESOURCES += ./src/WEDResources/scrollbar_corner.png
 RESOURCES += ./src/WEDResources/about.png
-RESOURCES += ./src/WEDResources/gateway.crt
 RESOURCES += ./src/WEDResources/COPYING
 RESOURCES += ./src/WEDResources/colors.png
 RESOURCES += ./src/WEDResources/taxi_sign.png
@@ -427,14 +426,6 @@ RESOURCES += ./src/WEDResources/tables/sider.png
 RESOURCES += ./src/WEDResources/tables/property_bar.png
 RESOURCES += ./src/WEDResources/tables/arrows.png
 RESOURCES += ./src/WEDResources/tables/delete.png
-#RESOURCES += ./src/WEDResources/world/earth_12.jpg
-#RESOURCES += ./src/WEDResources/world/earth_41.jpg
-#RESOURCES += ./src/WEDResources/world/earth_22.jpg
-#RESOURCES += ./src/WEDResources/world/earth_31.jpg
-#RESOURCES += ./src/WEDResources/world/earth_21.jpg
-#RESOURCES += ./src/WEDResources/world/earth_32.jpg
-#RESOURCES += ./src/WEDResources/world/earth_11.jpg
-#RESOURCES += ./src/WEDResources/world/earth_42.jpg
 RESOURCES += ./src/WEDResources/worldmap.jpg
 RESOURCES += ./src/WEDResources/pavement/dirt.png
 RESOURCES += ./src/WEDResources/pavement/gravel.png

src/WEDResources/gateway.crt
@@ -1,21 +1,0 @@
------BEGIN CERTIFICATE-----
-MIIDZjCCAk4CCQDaPERrxnQh9TANBgkqhkiG9w0BAQUFADB1MQswCQYDVQQGEwJV
-UzERMA8GA1UECBMIQ29sdW1iaWExFzAVBgNVBAcTDlNvdXRoIENhcm9saW5hMRkw
-FwYDVQQKExBMYW1pbmFyIFJlc2VhcmNoMR8wHQYDVQQDExZnYXRld2F5YXBpLngt
-cGxhbmUuY29tMB4XDTE1MDUyODE0MjYyN1oXDTQyMTAxMzE0MjYyN1owdTELMAkG
-A1UEBhMCVVMxETAPBgNVBAgTCENvbHVtYmlhMRcwFQYDVQQHEw5Tb3V0aCBDYXJv
-bGluYTEZMBcGA1UEChMQTGFtaW5hciBSZXNlYXJjaDEfMB0GA1UEAxMWZ2F0ZXdh
-eWFwaS54LXBsYW5lLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-AJyeuc8uoeLHBGx4O8rkpkDJJeJINGNnLy9A7I76BlxSzgKcPa8G07oYTKHw4Kb/
-SdG2Pg/n8+L40Z17HK1GFRpjscmOlInlsVe6i3Oik8zAawP8Ng/1mrRIxybbLtmx
-WQIaGNA9b5prZz8Prylz+ZbcvPu6FGdfh0XM00eRoBF22ok9z2gQa4WVPrQRs0lx
-IEh4hcN1i2oX0RpDRPCEt+1WZnwlmxJ8sm+PkyCJIlPiWgHsuK7Psh40oc5P3Edz
-Pq0jJBY4K1g5effW0W73xV0KLZBT/SctWVW+X3w8S7HyrHB4NEeDZN7FI5mKt3kT
-mfv4H5Zc5Ec6Jz43D4WH6/cCAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAjCFmWNMm
-USPfYMvrlw82p1EAVLCe3JZ10M/8kjhxFxjlofB8DSAAKwyaDFyepPvx4tblDwq2
-xNT9gyTHxDadV3bAix2fDGo6+MLgxiAkHJcIRjZhwDnmDPXzDPlR99uPo7it3nSD
-FB1Q5nTL7P4DPhdZUzicFX3UZ+q5sa7WIRp8ZNOEv26nzVyxzq6wzXmQyv3/Sk1l
-2ucxQhh4tyvuiBfLjGiIdWMCCohM05e/2V/veeQs4Udsdb/Pf38k6axu2p2qc4kR
-Eq+l3UH9cpky+xO06uSxNgVOoBDyA59+VgKJ4A8Qg5VqoUp4WNPWtNo0kqqtddC+
-BkvvNv59Zr4NJA==
------END CERTIFICATE-----

commit d89185f3916812df46184c9d0cb9bdd323989637

Fix: Restore correct tool shortcut keys.
These were broken by

https://github.com/X-Plane/xptools/commit/fdd8ad1547e608f952e121648c9a72181b72204c

src/WEDMap/WED_MapPane.cpp
@@ -70,13 +70,13 @@ char	kToolKeys[2*TOOLICON_ROWS] = {
 	0, 0,
 	0, 0, 0, 0,
 	0, 0, 0, 0,
+	0, 0, 0, 0,
 
 	'b', 0, 'a', 'o',
 	'e', 'w', 'f', 'g',
 
 	'l', 'k', 'h', 't',
-	'r', 's', 'v', 'm',
-	0, 0, 0, 0
+	'r', 's', 'v', 'm'
 };
 
 enum //Must be kept in sync with TabPane