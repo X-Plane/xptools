4ec79f39b40959ad0b2b17f9f8834c247160f655
meikelm <michael.minnhaar@yahoo.com>
2021-02-20 22:15:27 -0800

    undo accidential WED_Version.h change in last commit

--- src/WEDCore/WED_Version.h
+++ src/WEDCore/WED_Version.h
@@ -28,8 +28,8 @@
 // so you can't go using const int and other fancy-pants C++ stuff!
 
 // These versions are used in about boxes, resources, info boxes, etc.
-#define	WED_VERSION				2.3.1r1
-#define	WED_VERSION_STRING		"2.3.1r1"
+#define	WED_VERSION				2.4.0git_xp12_jetways
+#define	WED_VERSION_STRING		"2.4.0git_xp12_jetways"
 #define	WED_VERSION_STRING_SHORT	"2.4.0"			// omit beta/release number
 #define	WED_VERSION_RES			WED_VERSION_STRING
 #define	WED_VERSION_BIN			2,4,0,0
@@ -38,6 +38,6 @@
 // This numeric is used by the gateway to understand if our WED is up-to-date.
 // Format 1 digit major + 2 digit middle + 1 digit minor version + last digit
 // last digit is 0 for all beta versions or matches release version
-#define WED_VERSION_NUMERIC		20311
+#define WED_VERSION_NUMERIC		20400
 
 #endif /* WED_Version_H */
