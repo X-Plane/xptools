fb1a32fc6b755e44c57b18be5210e7035a981a47
meikelm <michael.minnhaar@yahoo.com>
2024-08-14 12:50:58 -0700

    prevent terrain object export slowing down TYLER_MODE exports

--- src/WEDImportExport/WED_DSFExport.cpp
+++ src/WEDImportExport/WED_DSFExport.cpp
@@ -1670,6 +1670,7 @@ static int	DSF_ExportTileRecursive(
 					if (!pts.empty())
 					{
 						++real_thingies;
+#if !TYLER_MODE
 						bool elevated = false;
 						// get DEM
 						Bbox2 bnds;
@@ -1691,7 +1692,6 @@ static int	DSF_ExportTileRecursive(
 						const for_info_t * fst_info = nullptr;
 						if (!WED_GetResourceMgr(resolver)->GetFor(r, fst_info))
 							elevated = false;
-
 						if(elevated)
 						{
 							cbs->BeginPolygon_f(idx, param, 4 + 10, writer);   // really means 4 data planes, but signals PointPool scaling needs to suit forests with height, msl
@@ -1710,6 +1710,7 @@ static int	DSF_ExportTileRecursive(
 							cbs->EndPolygon_f(writer);
 						}
 						else
+#endif
 							DSF_AccumPts(pts.begin(), pts.end(), safe_bounds, cbs, writer, idx, param);
 					}
 				}
