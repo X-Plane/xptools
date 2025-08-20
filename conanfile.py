dec554f411aaad5baf0f4a169b3061e8d949a1fa
Marco Auer <marco@x-plane.com>
2025-08-20 11:17:44 +0200

    Updated internal wed version and switched curl to openssl on Windows

--- conanfile.py
+++ conanfile.py
@@ -24,6 +24,8 @@ class XPToolsRecipe(ConanFile):
         self.requires("libcurl/8.15.0")
         self.requires("glew/2.2.0")
         self.requires("jsoncpp/1.9.6")
+        if self.settings.os == "Windows":
+            self.requires("openssl/3.3.2")
         if self.settings.os == "Linux":
             self.requires("fltk/1.4.1")
             self.requires("egl/system")
@@ -34,6 +36,9 @@ class XPToolsRecipe(ConanFile):
         self.options["libpng"].with_zlib = True
         self.options["glew"].shared = False
         self.options["glew"].with_glu = "system"
+
+        if self.settings.os == "Windows":
+            self.options["libcurl"].with_ssl = "openssl"
 
     def layout(self):
         cmake_layout(self)
