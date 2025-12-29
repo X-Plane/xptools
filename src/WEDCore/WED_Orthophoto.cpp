86d50619297175a64f625999454920dd7ad1a5b0
Ilyass Baba <ilyassbaba.me@gmail.com>
2025-12-29 23:07:55 +0100

    Delete the return for rng if create is nullptr because it was already handled and it may cause a silent failure

--- src/WEDCore/WED_Orthophoto.cpp
+++ src/WEDCore/WED_Orthophoto.cpp
@@ -95,10 +95,6 @@ WED_Ring * WED_RingfromImage(char * path, WED_Archive * arch, WED_MapZoomerNew *
 	WED_Ring * rng = WED_Ring::CreateTyped(arch);
 	rng->SetName("Image Boundary");
 
-
-	if (create != nullptr) return rng;
-
-
 	for (int i=0; i<4; ++i)
 	{
 		char s[12];
