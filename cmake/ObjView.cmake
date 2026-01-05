7272e39d73fb3f6f8ab77518fd6aee4f9d60a8dc
Stefan Riemens <stefan@x-plane.com>
2026-01-05 12:32:50 +0100

    Robots were kind enough to add header files to our targets, so they're part of the generated project files.

--- cmake/ObjView.cmake
+++ cmake/ObjView.cmake
@@ -1,32 +1,58 @@
 set(obj_view_sources
     src/Obj/XObjDefs.cpp
+    src/Obj/XObjDefs.h
     src/Obj/XObjReadWrite.cpp
+    src/Obj/XObjReadWrite.h
     src/Obj/ObjDraw.cpp
+    src/Obj/ObjDraw.h
     src/Obj/ObjPointPool.cpp
+    src/Obj/ObjPointPool.h
     src/ObjEdit/OE_Zoomer3d.cpp
+    src/ObjEdit/OE_Zoomer3d.h
     src/Utils/ObjUtils.cpp
+    src/Utils/ObjUtils.h
     src/Utils/AssertUtils.cpp
+    src/Utils/AssertUtils.h
     src/Utils/BitmapUtils.cpp
+    src/Utils/BitmapUtils.h
     src/Utils/EndianUtils.c
+    src/Utils/EndianUtils.h
     src/Utils/FileUtils.cpp
+    src/Utils/FileUtils.h
     src/Utils/GeoUtils.cpp
+    src/Utils/GeoUtils.h
     src/Utils/MatrixUtils.cpp
+    src/Utils/MatrixUtils.h
     src/Utils/MemFileUtils.cpp
+    src/Utils/MemFileUtils.h
     src/Utils/trackball.c
+    src/Utils/trackball.h
     src/Utils/TexUtils.cpp
+    src/Utils/TexUtils.h
     src/Utils/XUtils.cpp
+    src/Utils/XUtils.h
     src/Utils/unzip.c
+    src/Utils/unzip.h
     src/Utils/zip.c
+    src/Utils/zip.h
     src/UI/XGUIApp.cpp
+    src/UI/XGUIApp.h
     src/XPTools/ViewObj.cpp
 
     SDK/libtess2/Source/tess.c
+    SDK/libtess2/Source/tess.h
     SDK/libtess2/Source/bucketalloc.c
+    SDK/libtess2/Source/bucketalloc.h
     SDK/libtess2/Source/dict.c
+    SDK/libtess2/Source/dict.h
     SDK/libtess2/Source/mesh.c
+    SDK/libtess2/Source/mesh.h
     SDK/libtess2/Source/priorityq.c
+    SDK/libtess2/Source/priorityq.h
     SDK/libtess2/Source/sweep.c
+    SDK/libtess2/Source/sweep.h
     SDK/libtess2/Source/geom.c
+    SDK/libtess2/Source/geom.h
 )
 
 if (WIN32)
